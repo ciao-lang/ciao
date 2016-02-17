6f807c3189c39602bb933b421207c41d01343cea
Jose F. Morales <jfmcjf@gmail.com>
2016-02-17 11:13:48 +0100

    Public snapshot for a0132f392733cb76cd9929308eb25651a1171300
    Src-commit: a0132f392733cb76cd9929308eb25651a1171300

--- core/library/http_get/http_get.pl
+++ core/library/http_get/http_get.pl
@@ -1,21 +1,20 @@
 :- module(http_get, [http_get/2], [assertions]).
 
-:- doc(title, "Simple HTTP Download.").
+:- doc(title, "Simple HTTP Download").
 
 :- doc(author, "R@'{e}my Haemmerl@'{e}").
-:- doc(author, "The CLIP Group").
+:- doc(author, "Ciao Development Team").
 
-:- doc(module, "This module provides a simple interface for download
-        of files from the Web.  It supports HTTP, HTTPS, and FTP
-        protocols. 
+:- doc(module, "This module provides a simple interface for
+   downloading files from the Web. It supports HTTP, HTTPS, and FTP
+   protocols.
 
-        As backend the module uses the command line tool @tt{wget} or
-        @tt{curl}, depending on the one currently available on the
-         system.  Proxy are supported through the environment
-        variables, @tt{http_proxy}, @tt{https_proxy}, @tt{ftp_proxy},
-        and @tt{no_proxy}. Refer to the manual page of @tt{wget} or
-        @tt{curl} for more information about how setting those
-        variables.").
+   As backend the module uses the command line tool @tt{wget} or
+   @tt{curl}, depending on the one currently available on the system.
+   Proxy are supported through the environment variables,
+   @tt{http_proxy}, @tt{https_proxy}, @tt{ftp_proxy}, and
+   @tt{no_proxy}. Refer to the manual page of @tt{wget} or @tt{curl}
+   for more information about how setting those variables.").
 
 :- use_module(library(process)).
 :- use_module(library(process/process_channel)).
@@ -43,5 +42,6 @@ wget(URL, Output):-
 	process_call(path(wget), ['-q', URL, '-O', '-'], [stdout(Output)]).
 
 curl(URL, Output):-
-	process_call(path(curl), ['-s', URL], [stdout(Output)]).
+	% '-L' is needed to follow HTTP redirects
+	process_call(path(curl), ['-s', '-L', URL], [stdout(Output)]).
 
