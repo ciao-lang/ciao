f6283502ad6fd897b929db1e06b2840e4d641495
Jose F. Morales <jfmcjf@gmail.com>
2016-03-15 18:53:42 +0100

    Public snapshot for 33484fc605a06e7594c46135ed10029b0f7850ef
    Src-commit: 33484fc605a06e7594c46135ed10029b0f7850ef

--- core/library/doccomments/examples/markdown_test.pl
+++ core/library/doccomments/examples/markdown_test.pl
@@ -1,42 +1,40 @@
 :- module(markdown_test, [], [assertions,doccomments,regtypes,isomodes,fsyntax]).
 
-%!
-% @title Doccomments (with markdown) test
+%! \title Doccomments (with markdown) test
 %
-% @author Jose F. Morales 
+%  \author Jose F. Morales
 %
-% @summary This is a test file for the /lightweight/ mark-up language of
-%   @apl{LPdoc}. See @tt{markup_test.pl} for the long mark-up version.
+%  \summary This is a test file for the _lightweight_ mark-up language of
+%    \apl{LPdoc}. See \tt{markup_test.pl} for the long mark-up version.
 
-%!
-% @module
+%! \module
 %
-% @include{../../markdown/examples/markdown_simple.lpdoc}
+%  \include{../../markdown/examples/markdown_simple.lpdoc}
 %
-% ** More complex combinations
+%  ## More complex combinations
 %
-% (Check it from `doccomments_tests.pl`)
+%  (Check it from `doccomments_tests.pl`)
 
 % ===========================================================================
 % (for wiki only)
 
-%! @bug Adding more sections not working yet. That is:
-%    @begin{verbatim}
+%! \bug Adding more sections not working yet. That is:
+%    \begin{verbatim}
 %      /*!
-%      @tt{*} Same section
+%      \tt{#} Same section
 %     
 %      Text for the section.
 %      */
-%    @end{verbatim}
+%    \end{verbatim}
 % 
-%  @bug Adding more sections not working yet. That is:
-%    @begin{verbatim}
-%      % ! * Same section
+%  \bug Adding more sections not working yet. That is:
+%    \begin{verbatim}
+%      % ! # Same section
 %      % 
 %      %   Text for the section.
-%    @end{verbatim}
+%    \end{verbatim}
 
-%! @doinclude [foo/0, foo2/0, append/3, bar/2]
+%! \doinclude [foo/0, foo2/0, append/3, bar/2]
 
 %! foo/0: Testing itemizes inside the description of a predicate. Make
 %     sure that they are not interpreted as code verbatims:
@@ -74,32 +72,32 @@ append(_,_,_).
 %    which does even more things.
 bar(a,b).
 
-%! @doinclude [bar2/2]
+%! \doinclude [bar2/2]
 
 %! bar2(X,Y): This is a another predicate using `X` and `Y`,
 %    which does even more things.
 bar2(a,b).
 
 % ===========================================================================
-%! * Documentation comments in programs
+%! # Documentation comments in programs
 %
-%  Syntax for documentation comments is @tt{%!} and @tt{%<} for
+%  Syntax for documentation comments is \tt{%!} and \tt{%<} for
 %  documenting the preceding program element.
 %
 %  The following are some programs with comments:
 
-% %! @doinclude [color/1, weekday/1]
+% %! \doinclude [color/1, weekday/1]
 :- export([color/1, weekday/1]).
 
 %! color/1: The `color/1` regular type.
-%    @includedef{color/1}
+%    \includedef{color/1}
 :- regtype color/1.
 color := (red   %< First case. See `bar/0`.
      ) | (blue  %< Second case
      ).
 
 %! weekday/1: The `weekday/1` regular type.
-%    @includedef{weekday/1}
+%    \includedef{weekday/1}
 :- regtype weekday/1.
 weekday := (monday    /*< lunae dies. */)
 	 | (tuesday   /*< dies Martis. */)
@@ -107,16 +105,16 @@ weekday := (monday    /*< lunae dies. */)
          | (thursday  /*< Thor's day. */)
          | (friday    /*< day of Frigg. */).
 
-%! @bug Fix ugly parenthesis around doccomments
+%! \bug Fix ugly parenthesis around doccomments
 
 % ===========================================================================
-%! * Documentation of assertions in programs
-%  ** Complex assertions
+%! # Documentation of assertions in programs
+%  ## Complex assertions
 
-% %! @doinclude [len/2]
+% %! \doinclude [len/2]
 :- export([len/2]).
 
-%! Complex assertions can be included in special @tt{@assertions}
+%! Complex assertions can be included in special \tt{\assertions}
 %  blocks. The compiler can optionally read them if necessary (they
 %  are not merely documentation):
 
@@ -134,9 +132,8 @@ len(L, N) :- dlen(L, 0, N).
 llen(_,_,_). % incomplete
 dlen(_,_,_). % incomplete
 
-%!
-% @bug We need more compact ways of writing assertions (not only
-%   syntactic sugar, we need semantics for that). E.g.
+%! \bug We need more compact ways of writing assertions (not only
+%    syntactic sugar, we need semantics for that). E.g.
 %
 %       :- true pred length(L,N):
 %          ( list * var => list * int  %< Computes the length of `L`
@@ -150,7 +147,7 @@ dlen(_,_,_). % incomplete
 %          | list * int  %< Checks that `L` is of length `N`
 %          ).
 
-%! ** Simple assertions
+%! ## Simple assertions
 
 :- export([
 	len_0/2,
@@ -189,7 +186,7 @@ len_mt(_,_).
    the @var{L} argument (@bf{showing}: vars, modes, and types).".
 len_vmt(_,_).
 
-%! ** Combination of types and modes
+%! ## Combination of types and modes
 
 :- export([
 	sim_a/1, sim_a0/1, sim_a1/1, sim_a2/1, sim_a3/1,
@@ -213,11 +210,11 @@ sim_b0(_).
 :- pred sim_c(+list).
 sim_c(_).
 
-%% %! * Simpler assertions
+%% %! # Simpler assertions
 %% %
 %% %  Syntax for simpler assertions.
 %% %
-%% % TODO: Choose one of them
+%% % TODO: Disabled (it is hard to parse; use real assertions instead)
 %% 
 %% %@ pred length(+list,-int).
 %% %< Computes the length of the first argument.
@@ -244,7 +241,7 @@ sim_c(_).
 %% length(L, N) :- var(N), !, llength(L, 0, N).
 %% length(L, N) :- dlength(L, 0, N).
 %% 
-%% %! * Combining assertions with assertion-related predicates
+%% %! # Combining assertions with assertion-related predicates
 %% 
 %% %! This is the definition of a regular type `foo` and its code.
 %% 
@@ -277,19 +274,18 @@ sim_c(_).
 % ===========================================================================
 
 %! 
-% @bug Collect @tt{%! @@bug TEXT} and @tt{%< @@bug TEXT} doccomments
+% \bug Collect \tt{%! \\bug TEXT} and \tt{%< \\bug TEXT} doccomments
 %   that appear between the predicates.
 %
-% @bug We cannot add assertions in @tt{.lpdoc} files.
+% \bug We cannot add assertions in \tt{.lpdoc} files.
 %
-% @bug We cannot add @tt{:- if} directives in @tt{.lpdoc} files
+% \bug We cannot add \tt{:- if} directives in \tt{.lpdoc} files
 %
-% @bug Add labels.
-% @bug LPdoc does not support @tt{\} as escape character
-% @bug Need a verbatim with escape sequence
-% @bug The elements @tt{`word`} and @@tt@{word@} are different. Add wiki
+% \bug Add labels.
+% \bug Need a verbatim with escape sequence
+% \bug The elements \tt{`word`} and \\tt\{word\} are different. Add wiki
 %   syntax for it? (see Haddock).
 %
-% @bug How ` can be escaped? (is there any other option than
+% \bug How ` can be escaped? (is there any other option than
 %   using the long mark-up language?
 
