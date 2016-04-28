64ef925b660047c6d212a37b5d2732b4fadf3818
Jose F. Morales <jfmcjf@gmail.com>
2016-04-28 13:39:38 +0200

    Public snapshot for d6fbb6a96b2fa7b83e79c315c4fe3df5e8168abd
    Src-commit: d6fbb6a96b2fa7b83e79c315c4fe3df5e8168abd

--- core/library/http_get/http_get.pl
+++ core/library/http_get/http_get.pl
@@ -9,11 +9,11 @@
    downloading files from the Web. It supports HTTP, HTTPS, and FTP
    protocols.
 
-   As backend the module uses the command line tool @tt{wget} or
-   @tt{curl}, depending on the one currently available on the system.
+   As backend the module uses the command line tool @tt{curl} or
+   @tt{wget}, depending on the one currently available on the system.
    Proxy are supported through the environment variables,
    @tt{http_proxy}, @tt{https_proxy}, @tt{ftp_proxy}, and
-   @tt{no_proxy}. Refer to the manual page of @tt{wget} or @tt{curl}
+   @tt{no_proxy}. Refer to the manual page of @tt{curl} or @tt{wget}
    for more information about how setting those variables.").
 
 :- use_module(library(process)).
@@ -24,9 +24,9 @@
       output it in the channel @var{Output}.".
 
 http_get(URL, Output):-
-	detect_command(wget), !, wget(URL, Output).
-http_get(URL, Output):-
 	detect_command(curl), !, curl(URL, Output).
+http_get(URL, Output):-
+	detect_command(wget), !, wget(URL, Output).
 http_get(_, _) :- 
 	throw(error(http_get/2, neither_wget_nor_curl_found)).
 
@@ -38,10 +38,9 @@ detect_command(Cmd):-
 detect_command_(Cmd):-
 	process_call(path(Cmd), ['--version'], [stdout(null), stderr(null), status(0)]).
 
-wget(URL, Output):-
-	process_call(path(wget), ['-q', URL, '-O', '-'], [stdout(Output)]).
-
 curl(URL, Output):-
 	% '-L' is needed to follow HTTP redirects
 	process_call(path(curl), ['-s', '-L', URL], [stdout(Output)]).
 
+wget(URL, Output):-
+	process_call(path(wget), ['-q', URL, '-O', '-'], [stdout(Output)]).
