f5f712603f4541ebbd1e833bf82f73ad9f16dfc8
Jose F. Morales <jfmcjf@gmail.com>
2016-08-19 17:35:50 +0200

    Public snapshot for ed32a3549cf53433dbd2d915800a1f8aa7d55f72
    Src-commit: ed32a3549cf53433dbd2d915800a1f8aa7d55f72

--- contrib/library/block/block_example.pl
+++
@@ -1,8 +0,0 @@
-:- module(block_example, [merge/3], [block]).
-
-:- block merge(-,?,-), merge(?,-,-). 
-      
-merge([], Y, Y). 
-merge(X, [], X).  
-merge([H|X], [E|Y], [H|Z]) :- H @< E,  merge(X, [E|Y], Z).
-merge([H|X], [E|Y], [E|Z]) :- H @>= E, merge([H|X], Y, Z).
--- core/engine/term_typing_rtc.pl
+++
@@ -1,7 +0,0 @@
-:- module(term_typing_rtc,[rtc_ground/1],[]).
-
-% (rtcheck implementation for term_typing.pl)
-
-% rtcheck version for term_typing:ground/1
-
-rtc_ground(X) :- ground(X).
