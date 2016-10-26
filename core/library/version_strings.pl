1f770954116b40a9a06210cd0d27c9f8189d687b
Jose F. Morales <jfmcjf@gmail.com>
2016-10-26 17:55:40 +0200

    Public snapshot for 6afa6320c46a07f8e5835fed81bd3f0521e303c7
    Src-commit: 6afa6320c46a07f8e5835fed81bd3f0521e303c7

--- core/library/version_strings.pl
+++ core/library/version_strings.pl
@@ -1,23 +1,46 @@
 :- module(version_strings, [], [assertions]).
 
 :- doc(title, "Version string parsing and comparison").
+:- doc(author, "Jose F. Morales").
 
 :- doc(module, "This module contains operations to work with version
-   strings. The specification of the format and operations is
-   partially inspired on @href{http://semver.org/}{Semantic Versioning}.
-").
+   strings (as atoms). The specification of the format and operations
+   is partially inspired on @href{http://semver.org/}{Semantic
+   Versioning}.
 
-:- doc(bug, "Finish (add version comparison, parse prerelease and build fields)").
+   Normal versions are specified as strings of the form @tt{X.Y.Z},
+   where @tt{X}, @tt{Y}, and @tt{Z} are non-negative integers. The
+   components denote the major, minor, and patch version,
+   respectively. Missing version numbers are assumed to be 0.
+
+   Pre-release versions include an optional @em{pre-release}
+   identifier (@tt{X.Y.Z-Prerelease}), which itself may contain many
+   dot separated components.
+
+   Versions are compared by comparing each component from left to
+   right. Each component is compared numerically (if numbers) or
+   lexicographically (if not numbers). Numeric components have lower
+   precedence than non-numeric components. A pre-release version has
+   lower precedence than its corresponding normal version.").
+
+% TODO: Extend module interface as needed
+
+% TODO: 'git describe' is incompatible with semver, use a dot instead
+%   of hyphen to separate commit numbers from hash?
+
+% TODO: Add support for build metadata in versions (X.Y.Z-Prerelease+Build)
+%   Versions can include prerelease and build metadata
 
 :- use_module(library(lists), [append/3]).
 
 :- export(parse_version/3).
-% Obtain Major.Minor and Patch version numbers, as follows:
-%   'Major' -> 'Major', '0'
-%   'Major.Minor' -> 'Major.Minor', '0'
-%   'Major.Minor.Patch' -> 'Major.Minor', 'Patch'
+:- pred parse_version(VerAtm, VersionAtm, PatchAtm) # "Parse version
+   into coarse grained components (major and minor into
+   @var{VersionAtom}, patch and prerelease into @var{PathAtm})".
+
 parse_version(VerAtm, VersionAtm, PatchAtm) :-
-	atom_codes(VerAtm, Ver),
+	atom_codes(VerAtm, Ver0),
+	splitpre(Ver0, Ver, Pre),
 	splitdots(Ver, Vs),
 	( Vs = [] -> fail
 	; Vs = [Major|Vs0] ->
@@ -32,8 +55,11 @@ parse_version(VerAtm, VersionAtm, PatchAtm) :-
 	    )
 	; fail
 	),
+	( Pre = "" -> PatchPre = Patch
+	; append(Patch, "-"||Pre, PatchPre)
+	),
 	atom_codes(VersionAtm, Version),
-	atom_codes(PatchAtm, Patch).
+	atom_codes(PatchAtm, PatchPre).
 
 % Split string at dots (e.g., "1.2.3" => ["1","2","3"])
 splitdots([], []) :- !.
@@ -45,4 +71,60 @@ splitdots_([], [], []).
 splitdots_([0'.|Cs], [], Cs) :- !.
 splitdots_([C|Cs], [C|Ds], Cs2) :-
 	splitdots_(Cs, Ds, Cs2).
+
+:- export(version_compare/3).
+:- pred version_compare(C, A, B) # "Compare versions @var{A} and
+   @var{B} (see @pred{compare/3} for values of @var{C})".
+
+version_compare(C, A, B) :-
+	version_to_term(A, A2),
+	version_to_term(B, B2),
+	compare(C, A2, B2).
+
+%:- export(version_to_term/2).
+% Transform to term (for comparison using compare/3)
+version_to_term(Ver, Term) :-
+	atom_codes(Ver, Str),
+	splitpre(Str, VerN, VerPre),
+	( VerPre = "" -> Pres2 = none('','','') % greater than any list
+	; splitdots(VerPre, Pres),
+	  ids_to_terms(Pres, Pres2)
+	),
+	splitdots(VerN, Ns),
+	ids_to_terms(Ns, Ns2),
+	Term = Ns2-Pres2.    
+
+splitpre(Str, Norm, Pre) :-
+	( append(Norm, "-"||Pre0, Str), \+ Pre0 = "" -> Pre = Pre0
+	; Norm = Str, Pre = ""
+	).
+
+ids_to_terms([], []).
+ids_to_terms([X|Xs], [Y|Ys]) :- id_to_term(X, Y), ids_to_terms(Xs, Ys).
+
+id_to_term("0"||Cs, Id) :- !, Id = 0, Cs = "". % forbid leading zeroes
+id_to_term(Cs, Id) :- nonneg_int_codes(Id0, Cs), !, Id = Id0.
+id_to_term(Cs, Id) :- atom_codes(Id, Cs). % alphanum % TODO: missing check
+
+nonneg_int_codes(N, Cs) :-
+	number_codes(N, Cs),
+	integer(N),
+	N >= 0.
+	
+:- test test_norm # "Comparison of normal versions".
+test_norm :- strict_sorted_vers(['1.0.0', '2.0.0', '2.1.0', '2.1.1']).
+
+:- test test_pre # "Comparison of prerelease and normal versions (simple)".
+test_pre :- strict_sorted_vers(['1.0.0-alpha', '1.0.0']).
+
+:- test test_norm # "Comparison of prerelease versions (with subcomponents)".
+test_presub :- strict_sorted_vers([
+    '1.0.0-alpha', '1.0.0-alpha.1', '1.0.0-alpha.beta',
+    '1.0.0-beta', '1.0.0-beta.2', '1.0.0-beta.11',
+    '1.0.0-rc.1', '1.0.0']).
+
+% TODO: only for tests! disable when not testing
+strict_sorted_vers([]).
+strict_sorted_vers([_]) :- !.
+strict_sorted_vers([A,B|Xs]) :- version_compare(<, A, B), strict_sorted_vers([B|Xs]).
 
