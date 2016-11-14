c18d472f122a6aa9772e440f52dff280eaca575d
Jose F. Morales <jfmcjf@gmail.com>
2016-11-14 10:43:53 +0100

    Public snapshot for b2d95152b70be4fad0a11ecbcbe07ec02dcfba9f
    Src-commit: b2d95152b70be4fad0a11ecbcbe07ec02dcfba9f

--- builder/Manifest/Manifest.pl
+++ builder/Manifest/Manifest.pl
@@ -2,4 +2,10 @@
 version('1.2.0-alpha.2').
 depends([core]).
 alias_paths([ciaobld = 'src']).
-
+%
+cmd('ciao_builder', [main='cmds/ciao_builder']).
+%
+lib('src').
+lib('sh_src'). % TODO: not for windows!
+%
+manual('ciao_builder', [main='doc/SETTINGS.pl']).
--- doc/Manifest/Manifest.pl
+++ doc/Manifest/Manifest.pl
@@ -9,3 +9,9 @@ alias_paths([
   % TODO: not used yet?
   ciao_docsrc = '.'
 ]).
+%
+manual('ciao', [main='reference/SETTINGS.pl']).
+manual('ciao_devel', [main='developers/SETTINGS.pl']).
+readme('INSTALLATION', [main='common/INSTALLATION_CIAO']).
+readme('INSTALLATION_Win32', [main='common/INSTALLATION_CIAO_Win32']).
+readme('README', [main='common/README_CIAO']).
