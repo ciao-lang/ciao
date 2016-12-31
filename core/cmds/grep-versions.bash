5ca2d48bfdd3ae434f28d86c5ce2e58bfb3c1bb0
Jose F. Morales <jfmcjf@gmail.com>
2016-12-31 09:33:03 +0100

    Public snapshot for 34a8c1197b5becfa4dd12f22b792901eb82ce468
    Src-commit: 34a8c1197b5becfa4dd12f22b792901eb82ce468

---
+++ core/cmds/grep-versions.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Grep version on Ciao sources
+#
+# Author: Jose F. Morales
+# ---------------------------------------------------------------------------
+
+# Find in files with bundle version numbers
+function find_source() { # [paths]
+    local d
+    for d in "$@"; do
+	find_source_ "$d"
+    done
+}
+
+function find_source_() { # PATH
+    # one line per file, end in \0 (useful for xargs)
+    find "$1" \
+	 \( -name 'Manifest.pl' -o \
+	 -name '*.hooks.pl' -o \
+	 -name 'GlobalVersion' \) \
+	 -print0
+}
+
+regexp='[0-9][0-9]*\.[0-9][0-9]*'
+
+find_source "$@" | xargs -0 grep -I -nH -e "$regexp"
