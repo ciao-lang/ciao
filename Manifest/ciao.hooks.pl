b921b1a4cce58e3382bc78a93e1134868011271f
Jose F. Morales <jfmcjf@gmail.com>
2017-01-02 11:59:09 +0100

    Public snapshot for 647a502ad4c064461452395ccd56e3bacf818e6e
    Src-commit: 647a502ad4c064461452395ccd56e3bacf818e6e

--- Manifest/ciao.hooks.pl
+++ Manifest/ciao.hooks.pl
@@ -227,16 +227,3 @@ cmd_op(gmake) := gmake|make.
 ]).
 
 cmd_op(gtar) := gnutar|gtar|tar.
-
-% ===========================================================================
-:- doc(section, "Tests and Benchmarks").
-
-:- use_module(ciaobld(ciaoc_aux), [runtests_dir/2]).
-
-% TODO: accept input tag and tags per directory and do (DirTags |- FilterTags)
-%   e.g., [expensive] |- cheap is false
-%   e.g., [] |- cheap is true
-'$builder_hook'(test) :- !,
-	% Run ISO-prolog tests
-	% TODO: acceptance tests?
-	runtests_dir(ciao, 'tests/iso_tests').
