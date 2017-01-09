46b952eabebbb95bd7750ccb6e3fbddea4a52e36
Jose F. Morales <jfmcjf@gmail.com>
2017-01-09 16:57:24 +0100

    Public snapshot for 89b11ac503e4f91d6ce7f188e6c402de310af85c
    Src-commit: 89b11ac503e4f91d6ce7f188e6c402de310af85c

--- core/library/tcltk/examples_sicstus/hello_world.pl
+++
@@ -1,8 +0,0 @@
-:- use_module(library(tcltk)).
-
-go :-
-	tk_new([name('Example 1')], Interp),
-	tcl_eval(Interp, 'button .fred -text "hello world"
-		    -command { puts "hello world"}', _),
-	tcl_eval(Interp, 'pack .fred', _),
-	tk_main_loop.
