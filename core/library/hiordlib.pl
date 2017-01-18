6e97c0f13a6ef7c778c37027d1472ecc6c5aed27
Jose F. Morales <jfmcjf@gmail.com>
2017-01-18 10:52:47 +0100

    Public snapshot for ea6986df45f649218f4b69a20b2bf72803aee745
    Src-commit: ea6986df45f649218f4b69a20b2bf72803aee745

--- core/library/hiordlib.pl
+++ core/library/hiordlib.pl
@@ -2,6 +2,10 @@
      map/3, map/4, map/5,
      map/6, % NOTUSED
      foldl/4,
+     foldl/5,
+     foldl/6,
+     foldl/7,
+     foldl/8,
      foldr/4,
      minimum/3,
      filter/3,
@@ -73,10 +77,35 @@ map([X|Xs], [Y|Ys], [Z|Zs], P) --> P(X, Y, Z), map(Xs, Ys, Zs, P).
 % ---------------------------------------------------------------------------
 
 :- meta_predicate foldl(pred(3), ?, ?, ?).
+:- meta_predicate foldl(pred(4), ?, ?, ?, ?).
+:- meta_predicate foldl(pred(5), ?, ?, ?, ?, ?).
+:- meta_predicate foldl(pred(6), ?, ?, ?, ?, ?, ?).
+:- meta_predicate foldl(pred(7), ?, ?, ?, ?, ?, ?, ?).
+
 :- pred foldl(+P, ?Xs, ?V0, ?V)
    :: callable * list(term) * term * term
    # "Reduces (fold) @var{Xs} from the left applying @var{P} and using
       @var{V0}-@var{V} as accumulator.".
+%
+:- pred foldl(+P, ?Xs, ?Ys, ?V0, ?V)
+   :: callable * list(term) * list(term) * term * term
+   # "Like @pred{foldl/4} but applied to successive tuples
+      from @var{Xs}, @var{Ys}.".
+%
+:- pred foldl(+P, ?Xs, ?Ys, ?Zs, ?V0, ?V)
+   :: callable * list(term) * list(term) * list(term) * term * term
+   # "Like @pred{foldl/4} but applied to successive tuples
+      from @var{Xs}, @var{Ys}, @var{Zs}.".
+%
+:- pred foldl(+P, ?Xs, ?Ys, ?Zs, ?Us, ?V0, ?V)
+   :: callable * list(term) * list(term) * list(term) * list(term) * term * term
+   # "Like @pred{foldl/4} but applied to successive tuples
+      from @var{Xs}, @var{Ys}, @var{Zs}, @var{Us}.".
+%
+:- pred foldl(+P, ?Xs, ?Ys, ?Zs, ?Us, ?Ws, ?V0, ?V)
+   :: callable * list(term) * list(term) * list(term) * list(term) * list(term) * term * term
+   # "Like @pred{foldl/4} but applied to successive tuples
+      from @var{Xs}, @var{Ys}, @var{Zs}, @var{Us}, @var{Ws}.".
 
 :- doc(foldl/4, "
 The left fold family @tt{foldl/N} is equivalent to:
@@ -98,13 +127,46 @@ foldl(P, [X11,...,X1n], [Xm1,...,Xmn], V0, V) :-
 %    foldl: f(3, f(2, f(1, z)))
 
 foldl(P, Xs, V0, V) :-
-	foldl_(Xs, P, V0, V).
+	foldl1(Xs, P, V0, V).
+foldl(P, Xs, Ys, V0, V) :-
+	foldl2(Xs, P, Ys, V0, V).
+foldl(P, Xs, Ys, Zs, V0, V) :-
+	foldl3(Xs, P, Ys, Zs, V0, V).
+foldl(P, Xs, Ys, Zs, Us, V0, V) :-
+	foldl4(Xs, P, Ys, Zs, Us, V0, V).
+foldl(P, Xs, Ys, Zs, Us, Ws, V0, V) :-
+	foldl5(Xs, P, Ys, Zs, Us, Ws, V0, V).
 
-:- meta_predicate foldl_(?, pred(3), ?, ?).
-foldl_([], _P, V, V).
-foldl_([X|Xs], P, V0, V):-
+:- meta_predicate foldl1(?, pred(3), ?, ?).
+:- meta_predicate foldl2(?, pred(4), ?, ?, ?).
+:- meta_predicate foldl3(?, pred(5), ?, ?, ?, ?).
+:- meta_predicate foldl4(?, pred(6), ?, ?, ?, ?, ?).
+:- meta_predicate foldl5(?, pred(7), ?, ?, ?, ?, ?, ?).
+
+foldl1([], _P, V, V).
+foldl1([X|Xs], P, V0, V) :-
 	P(X, V0, V1),
-	foldl_(Xs, P, V1, V).
+	foldl1(Xs, P, V1, V).
+
+foldl2([], _P, [], V, V).
+foldl2([X|Xs], P, [Y|Ys], V0, V) :-
+	P(X, Y, V0, V1),
+	foldl2(Xs, P, Ys, V1, V).
+
+foldl3([], _P, [], [], V, V).
+foldl3([X|Xs], P, [Y|Ys], [Z|Zs], V0, V) :-
+	P(X, Y, Z, V0, V1),
+	foldl3(Xs, P, Ys, Zs, V1, V).
+
+foldl4([], _P, [], [], [], V, V).
+foldl4([X|Xs], P, [Y|Ys], [Z|Zs], [U|Us], V0, V) :-
+	P(X, Y, Z, U, V0, V1),
+	foldl4(Xs, P, Ys, Zs, Us, V1, V).
+
+foldl5([], _P, [], [], [], [], V, V).
+foldl5([X|Xs], P, [Y|Ys], [Z|Zs], [U|Us], [W|Ws], V0, V) :-
+	P(X, Y, Z, U, W, V0, V1),
+	foldl5(Xs, P, Ys, Zs, Us, Ws, V1, V).
 
 % (example)
 :- test foldl(P, Xs, V0, V) : (
@@ -121,7 +183,17 @@ foldl_([X|Xs], P, V0, V):-
    ) => (Y = c(1,c(2,c(3,c(4,nil))))) + (not_fails, is_det)
    # "Change list representation (tail-recursive)".
 
+:- test foldl(F, As, Bs, Cs, Cs0) : (
+     F = (''(A,B,Ys,Ys0) :- Ys=[(A,B)|Ys0]),
+     As = [1,2,3,4],
+     Bs = [a,b,c,d],
+     Cs0 = []
+   ) => (Cs = [(1,a),(2,b),(3,c),(4,d)]) + (not_fails, is_det)
+   # "Zip two lists (tail-recursive)".
+
 % ---------------------------------------------------------------------------
+
+% TODO: We only provide foldr/4 -- please add more versions if needed
 
 :- meta_predicate foldr(pred(3), ?, ?, ?).
 :- pred foldr(+F, ?Xs, +V0, ?V)
@@ -160,7 +232,7 @@ foldr(P, Xs, V0, V) :-
 
 :- meta_predicate foldr_(?, pred(3), ?, ?).
 foldr_([], _P, Z, Z).
-foldr_([X|Xs], P, Z, R):-
+foldr_([X|Xs], P, Z, R) :-
 	foldr_(Xs, P, Z, R1),
 	P(X, R1, R).
 
@@ -213,7 +285,7 @@ filter(Goal, List, Included) :-
 
 filter_([], _, []).
 filter_([X|Xs], P, Ys) :-
-	( call(P, X) ->
+	( P(X) ->
 	    Ys = [X|Ys0]
 	; Ys = Ys0
 	),
@@ -250,9 +322,34 @@ partition_([X|Xs], P, Ys, Zs) :-
 % maplist/N
 
 :- meta_predicate maplist(pred(1), ?).
+:- meta_predicate maplist(pred(2), ?, ?).
+:- meta_predicate maplist(pred(3), ?, ?, ?).
+:- meta_predicate maplist(pred(4), ?, ?, ?, ?).
+:- meta_predicate maplist(pred(5), ?, ?, ?, ?, ?).
+
 :- pred maplist(+P, +Xs) ::
 	(callable(P), list(Xs))
    # "@tt{P(X)} succeeds for each element @var{X} of @var{Xs}".
+%
+:- pred maplist(+P, +Xs, ?Ys) ::
+	(callable(P), list(Xs), list(Ys))
+   # "Like @pred{maplist/2} but applied to successive tuples
+      from @var{Xs}, @var{Ys}.".
+%
+:- pred maplist(+P, +Xs, ?Ys, ?Zs) ::
+	(callable(P), list(Xs), list(Ys), list(Zs))
+   # "Like @pred{maplist/2} but applied to successive tuples
+      from @var{Xs}, @var{Ys}, @var{Zs}.".
+%
+:- pred maplist(+P, +Xs, ?Ys, ?Zs, ?Vs) ::
+	(callable(P), list(Xs), list(Ys), list(Zs), list(Vs))
+   # "Like @pred{maplist/2} but applied to successive tuples
+      from @var{Xs}, @var{Ys}, @var{Zs}, @var{Vs}.".
+%
+:- pred maplist(+P, +Xs, ?Ys, ?Zs, ?Vs, ?Ws) ::
+	(callable(P), list(Xs), list(Ys), list(Zs), list(Vs), list(Ws))
+   # "Like @pred{maplist/2} but applied to successive tuples
+      from @var{Xs}, @var{Ys}, @var{Zs}, @var{Vs}, @var{Ws}.".
 
 :- doc(maplist/2, "
 The map list family @tt{maplist/N} is equivalent to:
@@ -264,30 +361,6 @@ maplist(P, [X11,...,X1n], [Xm1,...,Xmn]) :-
     P(Xn,  ..., Xmn).
 @end{verbatim}
 ").
-
-:- meta_predicate maplist(pred(2), ?, ?).
-:- pred maplist(+P, +Xs, ?Ys) ::
-	(callable(P), list(Xs), list(Ys))
-   # "Like @pred{maplist/2} but applied to successive tuples
-      from @var{Xs}, @var{Ys}.".
-%
-:- meta_predicate maplist(pred(3), ?, ?, ?).
-:- pred maplist(+P, +Xs, ?Ys, ?Zs) ::
-	(callable(P), list(Xs), list(Ys), list(Zs))
-   # "Like @pred{maplist/2} but applied to successive tuples
-      from @var{Xs}, @var{Ys}, @var{Zs}.".
-%
-:- meta_predicate maplist(pred(4), ?, ?, ?, ?).
-:- pred maplist(+P, +Xs, ?Ys, ?Zs, ?Vs) ::
-	(callable(P), list(Xs), list(Ys), list(Zs), list(Vs))
-   # "Like @pred{maplist/2} but applied to successive tuples
-      from @var{Xs}, @var{Ys}, @var{Zs}, @var{Vs}.".
-%
-:- meta_predicate maplist(pred(5), ?, ?, ?, ?, ?).
-:- pred maplist(+P, +Xs, ?Ys, ?Zs, ?Vs, ?Ws) ::
-	(callable(P), list(Xs), list(Ys), list(Zs), list(Vs), list(Ws))
-   # "Like @pred{maplist/2} but applied to successive tuples
-      from @var{Xs}, @var{Ys}, @var{Zs}, @var{Vs}, @var{Ws}.".
 
 maplist(P, Xs) :-
 	maplist1(Xs, P).
@@ -301,34 +374,34 @@ maplist(P, Xs, Ys, Zs, Vs, Ws) :-
 	maplist5(Xs, P, Ys, Zs, Vs, Ws).
 
 :- meta_predicate maplist1(?, pred(1)).
-:- meta_predicate maplist2(?, pred(1), ?).
-:- meta_predicate maplist3(?, pred(1), ?, ?).
-:- meta_predicate maplist4(?, pred(1), ?, ?, ?).
-:- meta_predicate maplist5(?, pred(1), ?, ?, ?, ?).
+:- meta_predicate maplist2(?, pred(2), ?).
+:- meta_predicate maplist3(?, pred(3), ?, ?).
+:- meta_predicate maplist4(?, pred(4), ?, ?, ?).
+:- meta_predicate maplist5(?, pred(5), ?, ?, ?, ?).
 
 maplist1([], _).
 maplist1([X|Xs], P) :-
-	call(P, X),
+	P(X),
 	maplist1(Xs, P).
 
 maplist2([], _, []).
 maplist2([X|Xs], P, [Y|Ys]) :-
-	call(P, X, Y),
+	P(X, Y),
 	maplist2(Xs, P, Ys).
 
 maplist3([], _, [], []).
 maplist3([X|Xs], P, [Y|Ys], [Z|Zs]) :-
-	call(P, X, Y, Z),
+	P(X, Y, Z),
 	maplist3(Xs, P, Ys, Zs).
 
 maplist4([], _, [], [], []).
 maplist4([X|Xs], P, [Y|Ys], [Z|Zs], [V|Vs]) :-
-	call(P, X, Y, Z, V),
+	P(X, Y, Z, V),
 	maplist4(Xs, P, Ys, Zs, Vs).
 
 maplist5([], _, [], [], [], []).
 maplist5([X|Xs], P, [Y|Ys], [Z|Zs], [V|Vs], [W|Ws]) :-
-	call(P, X, Y, Z, V, W),
+	P(X, Y, Z, V, W),
 	maplist5(Xs, P, Ys, Zs, Vs, Ws).
 
 
