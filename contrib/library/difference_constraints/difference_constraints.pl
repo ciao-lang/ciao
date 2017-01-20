fb9b608f76d4f63956d82eb0a8d3d4df0af8ef84
Jose F. Morales <jfmcjf@gmail.com>
2017-01-20 18:51:20 +0100

    Public snapshot for 4d7a325ac2f33ae7e52e43f6b29b51d819cf8b11
    Src-commit: 4d7a325ac2f33ae7e52e43f6b29b51d819cf8b11

--- contrib/library/difference_constraints/difference_constraints.pl
+++
@@ -1,8 +0,0 @@
-:- package(difference_constraints).
-
-:- use_package(runtime_ops).
-
-:- include(library(difference_constraints/difference_constraints_ops)).
-
-:- use_module(library(difference_constraints/difference_constraints_rt)).
-
