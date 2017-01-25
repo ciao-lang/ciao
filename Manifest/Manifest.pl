66c0dc92d9f591da54b79fa4d35349bfe9ffba86
Jose F. Morales <jfmcjf@gmail.com>
2017-01-25 13:13:08 +0100

    Public snapshot for 92d5c6d7fbdcf0a8f0cf145be33372970f380867
    Src-commit: 92d5c6d7fbdcf0a8f0cf145be33372970f380867

--- Manifest/Manifest.pl
+++ Manifest/Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(ciao).
 % Manifest file for the whole Ciao system
 packname('Ciao').
-version('1.16.0-alpha.1').
+version('1.16.0-alpha.2').
 alias_paths([
 %  ciao_tests = 'tests'
 %  ciao_examples = 'examples'
--- builder/Manifest/Manifest.pl
+++ builder/Manifest/Manifest.pl
@@ -1,5 +1,5 @@
 :- bundle(builder).
-version('1.3.0-alpha.1').
+version('1.4.0').
 depends([core]).
 alias_paths([ciaobld = 'src']).
 %
