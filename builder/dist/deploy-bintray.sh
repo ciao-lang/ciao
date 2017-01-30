b0c12187c7ea9c0de76fb362d56e74fd4617fd6b
Jose F. Morales <jfmcjf@gmail.com>
2017-01-30 12:08:31 +0100

    Public snapshot for d6ad6a729128ca5328909a0d6cc98bfbc290c7d4
    Src-commit: d6ad6a729128ca5328909a0d6cc98bfbc290c7d4

--- builder/dist/deploy-bintray.sh
+++ builder/dist/deploy-bintray.sh
@@ -14,14 +14,17 @@
 if [ x"$CIAOROOT" = x"" ]; then
     cat <<EOF
 ERROR: CIAOROOT not defined!
+
 EOF
     exit 1
 fi
-cd "$CIAOROOT"
+ciaoroot=$CIAOROOT
+
+cd "$ciaoroot"
 hostname -f > build/PREBUILT # Mark that this is a prebuilt version
 ./ciao-boot.sh gen_pbundle --kind=bin_tgz
-cd "$CIAOROOT"/build/pbundle
-VERS=$(cat "$CIAOROOT"/core/Manifest/GlobalVersion).$(cat "$CIAOROOT"/core/Manifest/GlobalPatch)
+cd "$ciaoroot"/build/pbundle
+VERS=$(cat "$ciaoroot"/core/Manifest/GlobalVersion).$(cat "$ciaoroot"/core/Manifest/GlobalPatch)
 PBUNDLE=$(echo Ciao-*.tar.gz | sed 's/Ciao-.*-bin/ciao-'$VERS'/')
 mv Ciao-*.tar.gz $PBUNDLE
 curl -T $PBUNDLE "$@" 'https://api.bintray.com/content/ciao-lang/builds/'$PBUNDLE';bt_package=ciao;bt_version='$VERS';publish=1;override=1'
