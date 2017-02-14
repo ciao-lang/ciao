f38a1b4a8f563ef0c728db7bed2c4e6a380097de
Jose F. Morales <jfmcjf@gmail.com>
2017-02-14 19:21:44 +0100

    Public snapshot for c7c80cad3bafcb8ea3bb90bcbf240e0cfce060fc
    Src-commit: c7c80cad3bafcb8ea3bb90bcbf240e0cfce060fc

--- core/Manifest/persdb_mysql.hooks.pl
+++ core/Manifest/persdb_mysql.hooks.pl
@@ -1,6 +1,7 @@
 % (included file)
 
 :- doc(section, "MySQL bundle").
+% TODO: move to its own bundle
 
 % ---------------------------------------------------------------------------
 % Configuration
@@ -27,78 +28,55 @@
         "MySQL has not been detected.  If you would like to use the\n"||
         "Ciao-MySQL interface it is highly recommended that you stop\n"||
         "the Ciao configuration now and install MySQL first."),
-    rule_default(WithMySQL, verify_mysql(WithMySQL)),
+    rule_default(HasMySQL, has_mysql(HasMySQL)),
     %
     interactive([advanced])
 ]).
 
-verify_mysql(Value) :-
+has_mysql(Value) :-
 	( mysql_installed -> Value = yes ; Value = no ).
 
 mysql_installed :-
 	detect_c_headers(['mysql/mysql.h']).
-
-:- bundle_flag(mysql_client_directory, [
-    comment("MySQL client library path"),
-    details(
-      % .....................................................................
-      "Directory where the MySQL client library is installed."),
-    needed_if(flag(with_mysql('yes'))),
-    rule_default(MySQLDir, get_mysql_dir(MySQLDir)),
-    %
-    interactive
-]).
-
-% TODO: This is not a good way of detecting MySQL
-get_mysql_dir(MySQL) :-
-	locate_file('libmysqlclient.a', MySQL),
-	!.
-get_mysql_dir('/usr/lib/mysql') :-
-	warning_message(
-	    "Unable to determine where the MySQL client library is " ||
-	    "installed.\nCurrent value (/usr/lib/mysql) is only a guess.").
-
-% TODO: Use mysql_config
-% TODO: REMOVE! 'locate' IS NOT available in many platforms (e.g., Mac OS X)
-locate_file(FileName, FileDir) :-
-	find_executable('locate', LocateExec), % (silently fail if no 'locate')
-	process_call(LocateExec, [FileName],
-	             [status(_), stdout(string(S))]),
-	atom_codes(FileName, SFileName),
-	append(SFileName, "\n", SFileNameN),
-	append(SFileDir,  "/" || SFileNameN, S),
-	atom_codes(FileDir, SFileDir),
-	!.
 
 % Note: libmysqlclient-dev in Debian
 
 % ---------------------------------------------------------------------------
 % Build
 
-:- use_module(ciaobld(builder_aux), [wr_template/4]).
+:- use_module(library(file_utils), [string_to_file/2]).
+:- use_module(library(llists), [flatten/2]).
 :- use_module(library(bundle/bundle_flags), [get_bundle_flag/2]).
 :- use_module(library(bundle/bundle_paths), [bundle_path/3]).
-:- use_module(library(system), [file_exists/1]).
+:- use_module(ciaobld(third_party_config), [foreign_config_var/4]).
+:- use_module(ciaobld(builder_aux), [add_rpath/3]).
 
 with_mysql := ~get_bundle_flag(core:with_mysql).
-mysql_client_directory := ~get_bundle_flag(core:mysql_client_directory).
 
 % Prepare source for build
 % (e.g., for automatically generated code, foreign interfaces, etc.)
 '$builder_hook'(persdb_mysql:prepare_build_bin) :-
+	mysql_prepare_bindings.
+
+mysql_auto_install(_) :- fail.
+m_bundle_foreign_config_tool(core, mysql, 'mysql_config').
+
+mysql_prepare_bindings :-
 	( with_mysql(yes) ->
-	    % TODO: ask bundle instead
-	    wr_template(origin,
-	        ~bundle_path(core, 'library/persdb_mysql'),
-	        'linker_opts_auto.pl',
-	        ['where_mysql_client_lives' = ~mysql_client_directory]),
-	    % TODO: why?
-	    ( file_exists(~bundle_path(core, 'library/persdb_mysql_op')) ->
-	        wr_template(origin,
-		    ~bundle_path(core, 'library/persdb_mysql_op'),
-		    'linker_opts_auto.pl',
-		    ['where_mysql_client_lives' = ~mysql_client_directory])
-	    ; true
-	    )
+	    normal_message("configuring MySQL library", []),
+ 	    foreign_config_var(core, mysql, 'cflags', CompilerOpts),
+ 	    foreign_config_var(core, mysql, 'libs', LinkerOpts1),
+	    ( mysql_auto_install(yes) ->
+	        % If installed as a third party, add ./third-party/lib
+	        % to the runtime library search path
+	        add_rpath(local_third_party, LinkerOpts1, LinkerOpts2)
+	    ; LinkerOpts2 = LinkerOpts1
+	    ),
+	    add_rpath(executable_path, LinkerOpts2, LinkerOpts),
+	    T = ~flatten([
+		    ":- extra_compiler_opts(\'"||CompilerOpts, "\').\n"||
+		    ":- extra_linker_opts(\'"||LinkerOpts, "\').\n"]),
+	    string_to_file(T, ~bundle_path(core, 'library/persdb_mysql/linker_opts_auto.pl'))
 	; true
 	).
+
