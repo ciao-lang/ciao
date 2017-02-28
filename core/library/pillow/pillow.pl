73d5ef79cfbad0465e33464e315e3198b9d1ad67
Isabel Garcia <isabel.garcia@imdea.org>
2017-02-28 16:02:10 +0100

    Public snapshot for 9d948ad3c1784781d4664c7e261d10436ec2c434
    Src-commit: 9d948ad3c1784781d4664c7e261d10436ec2c434

--- core/library/pillow/pillow.pl
+++ core/library/pillow/pillow.pl
@@ -2,4 +2,5 @@
 :- use_module(library(pillow/html)).
 :- use_module(library(pillow/http)).
 :- use_module(library(pillow/url)).
+:- use_module(library(pillow/cgi)).
 :- include(library(pillow/ops)).
