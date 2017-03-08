e811486b2bc7bed40e7d468162bf06a972b906c2
Jose F. Morales <jfmcjf@gmail.com>
2017-03-08 12:51:38 +0100

    Public snapshot for e8ea5a4b9f901def69aa5a6e51d5232f92dc8987
    Src-commit: e8ea5a4b9f901def69aa5a6e51d5232f92dc8987

--- core/lib/foreign_compilation.pl
+++ core/lib/foreign_compilation.pl
@@ -20,6 +20,8 @@
    in order to find out what are the proper compiler and linker to use, 
    and which options must be passed to them in the current architecture.").
 
+:- use_module(library(parse_shell_args), [parse_shell_args/2]).
+
 % NOTE: Automatically generated in build_engine.sh (eng_build_info.c)
 :- impl_defined(foreign_opts_cc/1).
 :- impl_defined(foreign_opts_ld/1).
@@ -34,7 +36,7 @@
 compiler_and_opts(CC, Opts):-
         foreign_opts_cc(CC),
 	foreign_opts_ccshared(Opts0),
-	split_blanks(Opts0, Opts).
+	parse_shell_args(Opts0, Opts).
 
 :- pred linker_and_opts(?LD, ?Opts) ::
         atm * list(atm)
@@ -44,30 +46,5 @@ compiler_and_opts(CC, Opts):-
 linker_and_opts(LD, Opts):-
         foreign_opts_ld(LD),
 	foreign_opts_ldshared(Opts0),
-	split_blanks(Opts0, Opts).
+	parse_shell_args(Opts0, Opts).
 
-% TODO: Move somewhere else?
-
-% Split blanks (e.g. 'a b c' -> ['a','b','c'])
-split_blanks(X, Ys) :-
-	atom_codes(X, Cs),
-	split(Cs, Zs),
-	atom_codes_list(Ys, Zs).
-
-split(Cs, Ys) :-
-	skip_blanks(Cs, Cs2),
-	( Cs2 = [] -> Ys = []
-	; split_(Cs2, As, As, Ys)
-	).
-
-split_([], As, [], [As]) :- !. % close As, end
-split_([0' |Xs], As, [], [As|Zs]) :- !, split(Xs, Zs). % close As, continue
-split_([X|Xs], As, [X|As0], Zs) :- split_(Xs, As, As0, Zs). % accum in As
-
-skip_blanks([0' |Xs], Ys) :- !, skip_blanks(Xs, Ys).
-skip_blanks(Xs, Xs).
-
-atom_codes_list([], []).
-atom_codes_list([X|Xs], [Cs|Css]) :-
-	atom_codes(X, Cs),
-	atom_codes_list(Xs, Css).
