51bc04778c5b498c8e0a13f9a58fa708e6f78ad3
Jose F. Morales <jfmcjf@gmail.com>
2018-07-13 20:35:01 +0100

    Public snapshot for 56629a6088a2dac7b21c108ba4a0f224c2a955a9
    Src-commit: 56629a6088a2dac7b21c108ba4a0f224c2a955a9

---
+++ core/lib/dynamic_clauses/dynamic_clauses_doc.pl
@@ -0,0 +1,33 @@
+:- use_package(assertions).
+:- doc(nodoc,assertions).
+
+:- doc(title,"Dynamic predicates (source preserving)").
+
+:- doc(author, "Daniel Cabeza").
+:- doc(author, "The Ciao Development Team").
+
+:- doc(module,"The package @lib{dynamic_clauses} provides the
+   assert/retract family of predicates to manipulate dynamic predicates.
+
+   The defined predicates (see @lib{dynamic_clauses_rt}) allow
+   modification of the program as it is actually running.  Clauses can
+   be added to the program (@em{asserted}) or removed from the program
+   (@em{retracted}), as well as inspected.  Note that in Ciao only the
+   dynamic predicates of the current module (or accessible dynamic
+   multifile predicates) can be accessed and modified.  This limits
+   the bad impact to global analysis of this dynamic modification of
+   the program.  Thus, if dynamic predicates are exported, to be able
+   to inspect or modify them externally some accessing predicates need
+   to be implemented and exported alongside.
+
+   For the inspecting/manipulating predicates, the argument which
+   corresponds to the clause head must be instantiated to an atom or a
+   compound term.  The argument corresponding to the clause must be
+   instantiated either to a term @tt{Head :- Body} or, if the body part
+   is empty, to @tt{Head}. An empty body part is represented as
+   @tt{true}.
+
+   Note that using this library is very detrimental to global analysis,
+   and that for most uses the predicates listed in
+   @ref{Fast/concurrent update of facts} suffice.").
+
