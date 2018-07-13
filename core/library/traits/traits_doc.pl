ba647b0e4e7419bdb1868d40571bc7d72eebba6c
Jose F. Morales <jfmcjf@gmail.com>
2018-07-13 03:12:54 +0200

    Public snapshot for 158ff335d20b0992e5143c153543df473b7e55b4
    Src-commit: 158ff335d20b0992e5143c153543df473b7e55b4

--- core/library/traits/traits_doc.pl
+++ core/library/traits/traits_doc.pl
@@ -1,23 +1,22 @@
 :- use_package(assertions).
 :- doc(nodoc,assertions).
 
-:- doc(title,"Traits (experimental)").
+:- doc(title,"Traits").
 
 :- doc(author, "Jose F. Morales").
 
+:- doc(stability, devel("Currently the subject of active development
+   and research. Syntax may change without warning or deprecation
+   period.")).
+
 :- doc(module, "This package extends the Ciao module system with
    traits. This is a lightweight translation with no overhead
-   w.r.t. traditional use of multifile hook predicates.
+   w.r.t. traditional use of @tt{multifile} declarations for @em{hook}
+   predicates.
 
    A @concept{trait} is defined in Ciao as a collection of predicates
    that can be implemented for any functor. Functors can implement
    multiple traits.
-
-   @begin{alert}
-   Use with care. The syntax for this functionality is still not
-   stable. Better syntax (and syntactic sugar) may be included in the
-   future.
-   @end{alert}
 
    This translation delegates on the underlying module system as much
    as possible, e.g., for dealing with undefined predicates.
