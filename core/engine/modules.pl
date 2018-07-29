bc6b68db4c00bdac9a7ca14a2b8fd06e5981979f
Jose F. Morales <jfmcjf@gmail.com>
2018-07-29 03:38:13 +0200

    (alldocs) merged loading_code.pl into modules.pl (see below)
    The documentation at loading_code fits nicely into the module system
    description (using packages and importing 'user' files). No need for a
    separate section.
    
    Src-commit: 623f4431dc35790e1ecc82b41b9167fcc648882a

--- core/engine/modules.pl
+++ core/engine/modules.pl
@@ -149,6 +149,23 @@
 
 % ---------------------------------------------------------------------------
 
+:- doc(doinclude,use_package/1).
+:- doc(use_package(Package),"Specifies the use in this file of the
+          packages defined in @var{Package}.  See the description of the
+          third argument of @decl{module/3} for an explanation of
+          @concept{package file}s.
+
+          This directive must appear the first in the file, or just
+          after a @decl{module/3} declaration.  A file with no module
+          declaration, in the absence of this directive, uses an
+          implicit package @tt{default} (see @ref{Other predicates and
+          features defined by default}).").
+
+:- decl use_package(Package) : sourcename.
+:- decl use_package(Package) : list(sourcename).
+
+% ---------------------------------------------------------------------------
+
 :- doc(doinclude,use_module/2).
 :- decl use_module(Module, Imports) : sourcename * list(predname)
         # "Specifies that this code imports from the module defined in
@@ -207,6 +224,26 @@ main(X) :-
           this module imports from the module defined in @var{Module}
           all the predicates exported by it, an also that this module
           exports all such predicates .".
+
+:- doc(doinclude,ensure_loaded/1).
+:- decl ensure_loaded(File) : sourcename + iso
+        # "Specifies that the code present in @var{File} will be
+          included in the executable being prepared, in the @tt{user}
+          module.  The file @var{File} cannot have a module
+          declaration.  This directive is intended to be used by
+          programs not divided in modules.  Dividing programs into
+          modules is however strongly encouraged, since most of the
+          attractive features of Ciao (such as static debugging and
+          global optimization) are only partially available for
+          @tt{user} modules.".
+
+% ---------------------------------------------------------------------------
+
+:- doc(doinclude,include/1).
+:- decl include(File) : sourcename + iso
+        # "The contents of the file @var{File} are included in the
+          current program text exactly as if they had been written in
+          place of this directive.".
 
 % ---------------------------------------------------------------------------
 
