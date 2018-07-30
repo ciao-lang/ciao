051473eff15a856ff515ba529edd980bb9f2648c
Jose F. Morales <jfmcjf@gmail.com>
2018-07-30 17:51:30 +0200

    (core) runtime_ops moved to library/, added warning on global extension
    Src-commit: 81d54fd96d87f21177230172c3d1a53e3edd055a

--- core/library/runtime_ops/runtime_ops_doc.pl
+++ core/library/runtime_ops/runtime_ops_doc.pl
@@ -9,6 +9,17 @@
    defined operators of a module for the reading performed at run-time
    by the program that uses the module. Simply by using this package the 
    operator definitions appearing in the module are enabled during the 
-   execution of the program.").
+   execution of the program.
+
+   @begin{alert}
+   @bf{Use with care:} this is a non-modular extension. It may alter
+   the behaviour of other modules (performing read/write operations)
+   not using the package. This may be improved in future versions of
+   the package (see bugs entries below).
+   @end{alert}
+   ").
+
+:- doc(bug, "Consider implementing local runtime operator tables and
+   @tt{addmodule} versions of read/write predicates").
 
 :- use_package(runtime_ops).
