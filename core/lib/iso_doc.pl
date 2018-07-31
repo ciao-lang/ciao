e6f53cecac2741b9c8068dc7fde291954815aa63
Jose F. Morales <jfmcjf@gmail.com>
2018-07-31 05:39:23 +0200

    (alldocs) defects in ISO compliance moved from intro to iso package documentation
    Src-commit: 83156d9d546f6bfc88ea8d92e2440dbbd6fe69df

--- core/lib/iso_doc.pl
+++ core/lib/iso_doc.pl
@@ -7,6 +7,21 @@
 
 :- doc(author, "The Ciao Development Team").
 
-:- doc(module,"This library package allows the use of the
-   @concept{ISO-Prolog} predicates in Ciao programs. The compatibility
-   is not at 100\% yet.").
+:- doc(module,"This package enables by default
+   @concept{ISO-Prolog} predicates in Ciao programs.
+
+   Compliance with ISO is still not complete: currently there are some
+   minor deviations in, e.g., the treatment of characters, the syntax,
+   some of the arithmetic functions, and part of the error system.
+   Also, Ciao does not offer a strictly conforming mode which rejects
+   uses of non-ISO features. 
+").
+
+% @comment{Given that the final version of the ISO standard has only
+% been recently published,}
+
+% @comment{On the other hand, Ciao has been reported by independent
+% sources (members of the standarization body) to be one of the most
+% conforming Prologs at the moment of this writing, and the first one
+% to be able to compile all the standard-conforming test cases.}
+   
