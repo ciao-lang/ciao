ebc05cc36891760bdbeb188f2b142313edb91cb9
Jose F. Morales <jfmcjf@gmail.com>
2018-08-04 02:49:03 +0200

    (core) Marked in nonpure which mods should not be added by default
    Src-commit: 083655c07d7648875f987406285184299f409f85

--- core/lib/nonpure.pl
+++ core/lib/nonpure.pl
@@ -4,19 +4,19 @@
 
 :- use_module(engine(arithmetic)).
 :- use_module(engine(atomic_basic)).
-:- use_module(engine(basic_props)).
+:- use_module(engine(basic_props)). % TODO: only in assertions
 :- use_module(engine(basiccontrol)).
 :- use_module(engine(data_facts)).
 :- use_module(engine(exceptions)).
-:- use_module(engine(io_aux)).
-:- use_module(engine(io_basic)).
-:- use_module(engine(prolog_flags)).
-:- use_module(engine(streams_basic)).
-:- use_module(engine(system_info)).
+:- use_module(engine(io_aux)). % TODO: not by default
+:- use_module(engine(io_basic)). % TODO: not by default
+:- use_module(engine(prolog_flags)). % TODO: not by default
+:- use_module(engine(streams_basic)). % TODO: not by default
+:- use_module(engine(system_info)). % TODO: not by default
 :- use_module(engine(term_basic)).
 :- use_module(engine(term_compare)).
 :- use_module(engine(term_typing)).
-:- use_module(engine(hiord_rt), [call/1]).
-:- use_module(engine(debugger_support), [srcdbg_spy/7]).
+:- use_module(engine(hiord_rt), [call/1]). % TODO: not by default
+:- use_module(engine(debugger_support), [srcdbg_spy/7]). % TODO: not by default? (except if internal)
 
 :- use_package(condcomp).
