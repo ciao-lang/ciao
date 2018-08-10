cb59d1d908a668e506c00f2dc6bd261d0c31c886
Jose F. Morales <jfmcjf@gmail.com>
2018-08-10 13:10:11 +0200

    (core) Documented library(listing) (a compatibility module)
    Src-commit: d92d18870160851340ec909efbecdd3fb7e499f6

--- core/library/listing.pl
+++ core/library/listing.pl
@@ -1,4 +1,16 @@
-:- module(listing, [listing/0, listing/1],[assertions]).
+:- module(listing, [listing/0, listing/1], [assertions, isomodes]).
+
+:- doc(title, "Printing dynamic predicates").
+:- doc(author, "The Ciao Development Team").
+
+:- doc(module, "This module implements predicates to pretty print the
+   contents of the dynamic predicate database to the output
+   stream. These predicates are introduced for compatibility with
+   legacy C-Prolog, as well as modern Prolog implementations. Note
+   that the predicate representation may change due to internal code
+   expansions.").
+
+% E.g. https://www2.cs.duke.edu/csl/docs/cprolog.html
 
 :- use_module(engine(internals), 
         ['$current_clauses'/2,
@@ -12,11 +24,17 @@
 % TODO: load in the toplevel by default?
 % TODO: combine with dynamic_clauses package
 
+:- pred listing # "Show the definition of all the dynamic predicates
+   using @pred{portray_clause/1}.".
+
 listing :-
 	'$current_predicate'(_, Pred),
 	listing1(Pred),
 	fail.
 listing.
+
+:- pred listing(+Spec) # "Show the definition the specified predicate given
+   by @var{Spec} (@tt{F/A}) using @pred{portray_clause/1}.".
 
 listing(Arg) :-
 	parse_functor_spec(Arg, user, X, listing1(X), listing/1-1).
