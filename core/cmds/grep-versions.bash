06cb3ad0944ff57f4b909c8089d87e85f85535eb
Jose F. Morales <jfmcjf@gmail.com>
2018-08-30 19:58:33 +0200

    (core) Updated M-x ciao-grep-versions-all to show internal version numbers
    Those numbers are the internal compiler version count (useful for
    bootstrapping), version of po files, and version of itf files.
    
    Src-commit: ce9ecc8e87ca7febb895897a8d6a0733e6446ecd

--- core/cmds/grep-versions.bash
+++ core/cmds/grep-versions.bash
@@ -5,23 +5,44 @@
 # ---------------------------------------------------------------------------
 
 # Find in files with bundle version numbers
-function find_source() { # [paths]
+function find_vers() { # [paths]
     local d
     for d in "$@"; do
-	find_source_ "$d"
+	find_vers_ "$d"
     done
 }
 
 # TODO: GlobalPatch is not shown (and it does ot match regexp)
-function find_source_() { # PATH
+function find_vers_() { # PATH
     # one line per file, end in \0 (useful for xargs)
     find "$1" \
 	 \( -name 'Manifest.pl' -o \
 	 -name '*.hooks.pl' -o \
 	 -name 'GlobalVersion' \) \
 	 -print0
+    # other interesting files
 }
 
-regexp='[0-9][0-9]*\.[0-9][0-9]*'
+# Something that looks like a version number
+vers_regexp='[0-9][0-9]*\.[0-9][0-9]*'
 
-find_source "$@" | xargs -0 grep -I -nH -e "$regexp"
+# Other version numbers
+function find_others() { # [paths]
+    local d
+    local f
+    for d in "$@"; do
+	for f in \
+	    core_OC/compiler/frontend_common.pl \
+	    core/engine/internals.pl \
+	    core/lib/compiler/c_itf_internal.pl;
+	do
+	    [ -r "$d"/"$f" ] && printf "%s\0" "$d"/"$f"
+	done
+    done
+}
+
+# Other versions
+others_regexp='\(\<compiler_version\>\|\<poversion\>\|\<itf_version\>\)(.*[0-9]'
+
+find_vers "$@" | xargs -0 grep -I -nH -e "$vers_regexp"
+find_others "$@" | xargs -0 grep -I -nH -e "$others_regexp"
