1a3ae9c21277fef394324fd703357c02a69b8e35
Jose F. Morales <jfmcjf@gmail.com>
2019-04-23 13:19:23 +0200

    (optim-comp) completed backport of new prelude.pl
    Src-commit: e2902dde1b8458be2848e509360ed33c1c2a4ff8

--- core/lib/hiord.pl
+++ core/lib/hiord.pl
@@ -1,3 +1,7 @@
 :- package(hiord).
 :- set_prolog_flag(read_hiord, on).
 :- use_module(engine(hiord_rt)).
+
+:- if(defined(optim_comp)).
+:- use_module(engine(rt_exp), [rt_pgcall/2, rt_modexp/4, rt_exp/6]). % TODO: put a $ in the name % Opt?
+:- endif.
--- core/lib/prelude.pl
+++ core/lib/prelude.pl
@@ -9,9 +9,15 @@
 :- use_module(engine(arithmetic)).
 :- use_module(engine(atomic_basic)).
 :- use_module(engine(basiccontrol)).
+:- if(defined(optim_comp)).
+:- use_module(engine(interpreter)). % TODO: make it optional?
+:- endif.
 :- use_module(engine(exceptions)).
 :- use_module(engine(term_basic)).
 :- use_module(engine(term_compare)).
 :- use_module(engine(term_typing)).
+:- if(defined(optim_comp)).
+:- else.
 :- use_module(engine(debugger_support), [srcdbg_spy/7]). % TODO: internal, make it optional or hide
+:- endif.
 
