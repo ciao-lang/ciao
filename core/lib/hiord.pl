fc82d4320ac602637f0fb46abdfb813a39486740
Jose F. Morales <jfmcjf@gmail.com>
2019-04-25 11:44:08 +0200

    (core) Disabled use of condcomp declaration in hiord.pl (until it is really integrated into the compiler)
    Src-commit: 834297c2920327723af8ece9fae3d517083ab320

--- core/lib/hiord.pl
+++ core/lib/hiord.pl
@@ -2,6 +2,7 @@
 :- set_prolog_flag(read_hiord, on).
 :- use_module(engine(hiord_rt)).
 
-:- if(defined(optim_comp)).
-:- use_module(engine(rt_exp), [rt_pgcall/2, rt_modexp/4, rt_exp/6]). % TODO: put a $ in the name % Opt?
-:- endif.
+% TODO: this cannot be safely enabled until condcomp is integrated into the compiler
+% :- if(defined(optim_comp)).
+% :- use_module(engine(rt_exp), [rt_pgcall/2, rt_modexp/4, rt_exp/6]). % TODO: put a $ in the name % Opt? % TODO: make it really optional
+% :- endif.
