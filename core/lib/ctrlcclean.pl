3a4be6048f2ac4531802cfd6878273185e9c341d
Jose F. Morales <jfmcjf@gmail.com>
2019-04-26 09:55:43 +0200

    (optim-comp) merged ctrlcclean.pl
    Src-commit: e34ed7fd490b2b7e790b039c2ac7b22255156045

--- core/lib/ctrlcclean.pl
+++ core/lib/ctrlcclean.pl
@@ -1,22 +1,33 @@
-:- module(ctrlcclean, [ctrlc_clean/1, delete_on_ctrlc/2, ctrlcclean/0],
-	[assertions, datafacts]).
+:- module(ctrlcclean, [], [noprelude, datafacts]).
+
+% TODO: incomplete, make it more robust (handle signals INT, TERM)
+
+:- use_module(engine(basiccontrol)).
+:- use_module(engine(term_basic)).
+:- use_module(engine(exceptions)).
+:- if(defined(optim_comp)).
+:- use_module(engine(rt_exp), [rt_modexp/4]).
+:- endif.
 
 :- use_module(library(system), [delete_file/1, working_directory/2]).
 
+:- export(ctrlc_clean/1).
 :- meta_predicate(ctrlc_clean(goal)).
-
 ctrlc_clean(Goal) :- intercept(Goal, control_c, ctrlcclean).
 
-:- data delOnCtrlC/2.
+:- data del_on_ctrlc/2.
 
-% todo: (jfran) when is delOnCtrlC retracted?
+% TODO: document usage, it must be ended with erase/1
+:- export(delete_on_ctrlc/2).
 delete_on_ctrlc(File, Ref) :-
         working_directory(Dir, Dir),
-        asserta_fact(delOnCtrlC(Dir, File), Ref).
+        asserta_fact(del_on_ctrlc(Dir, File), Ref).
 
+:- export(ctrlcclean/0).
 ctrlcclean :-
-        retract_fact(delOnCtrlC(Dir, File)),
+        retract_fact(del_on_ctrlc(Dir, File)),
         working_directory(_, Dir),
         delete_file(File),
         fail.
 ctrlcclean :- halt.
+
