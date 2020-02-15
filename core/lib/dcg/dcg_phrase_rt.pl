8443463b97a58dbad9eb5e717772d0ab0816c60b
Jose F. Morales <jfmcjf@gmail.com>
2020-02-15 19:58:02 +0100

    (core_OC) support conditional code in toplevel, merged default_for_ciaosh package
    Src-commit: e4bb13067661a1b5e9306f6a844a05d682743621

--- core/lib/dcg/dcg_phrase_rt.pl
+++ core/lib/dcg/dcg_phrase_rt.pl
@@ -1,15 +1,13 @@
-:- module(dcg_phrase_rt, [phrase/2, phrase/3], 
-    [assertions, nortchecks, isomodes]).
-
-:- use_module(engine(internals), [module_concat/3]).
-:- use_module(library(dcg/dcg_tr), [dcg_translate_dcg/5]).
+:- module(dcg_phrase_rt, [phrase/2, phrase/3], [assertions, nortchecks, isomodes]).
 
 :- doc(title,"Runtime support for DCGs").
-
 :- doc(author, "Jose F. Morales").
 
 :- doc(module, "This module offers translation of Definite Clause
    Grammars (DCGs) at runtime.").
+
+%:- use_module(engine(internals), [module_concat/3]).
+:- use_module(library(dcg/dcg_tr), [dcg_translate_dcg/5]).
 
 % (added by dcg_phrase when needed)
 :- multifile('\6\call_in_module'/2).
@@ -20,6 +18,17 @@
 %:- meta_predicate phrase(goal,?,?).
 
 :- doc(phrase(Phrase,List),"Like @tt{phrase(Phrase,List,[])}.").
+
+:- if(defined(optim_comp)).
+:- '$context'(phrase/2, module).
+phrase(P, _) :-
+    var(P), !,
+    throw(error(instantiation_error, phrase/2-1)).
+phrase(P, L) :-
+    '$module'(M), 
+    dcg_translate_dcg(P, P1, M, L, []), !,
+    '\6\call_in_module'(M, P1).
+:- else.
 :- meta_predicate phrase(addmodule,?).
 phrase(P, _, _) :-
     var(P), !,
@@ -27,12 +36,23 @@ phrase(P, _, _) :-
 phrase(P, M, L) :-
     dcg_translate_dcg(P, P1, M, L, []), !,
     '\6\call_in_module'(M, P1).
+:- endif.
 
 :- pred phrase(+Phrase,?List,?Remainder)
 # "The list @var{List} is a phrase of type @var{Phrase} (according to
    the current grammar rules), where @var{Phrase} is either a
    non-terminal or more generally a grammar rule body. @var{Remainder}
    is what remains of the list after a phrase has been found.".
+:- if(defined(optim_comp)).
+:- '$context'(phrase/3, module).
+phrase(P, _, _) :-
+    var(P), !,
+    throw(error(instantiation_error, phrase/3-1)).
+phrase(P, L, R) :-
+    '$module'(M), 
+    dcg_translate_dcg(P, P1, M, L, R), !,
+    '\6\call_in_module'(M, P1).
+:- else.
 :- meta_predicate phrase(addmodule,?,?).
 phrase(P, _, _, _) :-
     var(P), !,
@@ -40,6 +60,7 @@ phrase(P, _, _, _) :-
 phrase(P, M, L, R) :-
     dcg_translate_dcg(P, P1, M, L, R), !,
     '\6\call_in_module'(M, P1).
+:- endif.
 
 
 
