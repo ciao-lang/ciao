1ea59b1cc0486100dd005c94a8e505dfa533d373
Manuel Hermenegildo <manuel.hermenegildo@imdea.org>
2020-03-07 15:38:54 +0100

    (core) Improved dcoumentation of regexp match.
    Src-commit: 71d8a7fa08fb4020e26dbec060d485dd769522ed

--- core/library/regexp/regexp_doc.pl
+++ core/library/regexp/regexp_doc.pl
@@ -24,22 +24,23 @@
     replace_all/4
                   ]).
 
-:- doc(module,"This library provides facilities for matching
-   strings and terms against @index{patterns}. There are some @concept{prolog flags}
-  @begin{itemize}
+:- doc(module,"This library provides facilities for matching strings
+   and terms against @index{patterns}. Some @concept{prolog
+   flags} are available to control its behavior.
 
-   @item There is a @concept{prolog flag} to case insensitive
-   match. Its name is case_insensitive. If its value is on, matching is
-   case insenseitive, but if its value is off matching isn't case
-   insensitive. By default, its value is off.
+   @begin{itemize}
 
-   @item There is a syntax facility to use matching more or less like
-   a unification. You can type, \" =~ \"regexp\" \" as an argument of
-   a predicate. Thus, that argument must match with regexp. For example:
- 
- @begin{verbatim}
-      pred ( =~ \"ab*c\", B) :- ...    
- @end{verbatim}
+   @item The flag @tt{@concept{case_insensitive}} controls whether to
+   perform case-insensitive (@tt{on}) or case-sensitive (@tt{off})
+   match. The default value is @tt{off}.
+
+   @item There is a syntax facility that allows using this regular
+   expression matching in a similar way to unification. You can use 
+   @tt{=~ \"regexp\"} as an argument of a predicate, and then that
+   argument must match with the regexp. For example: 
+@begin{verbatim}
+   pred ( =~ \"ab*c\", B) :- ...  
+@end{verbatim}
 
 is equivalent to
 
@@ -47,36 +48,46 @@ is equivalent to
       pred (X,B) :- match_posix(\"ab*c\",X,R), ...
  @end{verbatim}
 
-    So, there are two @concept{prolog flags} about this. One of this
-    @concept{prolog flags} is \"format\". Its values are shell, posix,
-    list and pred, and sustitute in the example match_posix by
-    match_shell, match_posix, match_struct and macth_pred respectivly. By
-    default its value is posix.  The other @concept{prolog flag} is
-    exact. Its values are on and off. If its value is off sustitute in the
-    example R by []. If its value is on, R is a variable. By default, its
-    value is on.  
+    Two additional flags control this matching. The first one is
+    @tt{@concept{format}}. Its values are @tt{shell}, @tt{posix},
+    @tt{list} and @tt{pred}.  Their effect is as if changing in the
+    example above the call to @pred{match_posix/3} by a call to,
+    respectively, @tt{match_shell/2}, @tt{match_posix/3},
+    @tt{match_struct/3}, and @tt{match_pred/3}.  The default value is
+    @tt{posix}.  The other prolog flag is @tt{@concept{exact}}. Its
+    values are @tt{on} and @tt{off}. The @tt{off} value means
+    replacing in the example @tt{R} with @tt{[]}. If the value is
+    @tt{on}, then @tt{R} is a variable. The default value is @tt{on}.
 
 @end{itemize}").
 
 :- doc(regexp_shell/1,
     "Special characters for @var{regexp_shell} are:
   @begin{description}
-  @item{*} Matches any string that not begin by the character '.'.
-  @item{?} Matches any single character whenever it is not a starting point.
-  @item{[...]} Matches any one of the enclosed characters.  A pair of
-   characters separated by a minus sign denotes a range; any character
-   lexically between those two characters, inclusive, is matched.  If the
-   first character following the [ is a ^ then any character not enclosed
-   is matched.  No other character is special inside this construct.  To
-   include a ] in a character set, you must make it the first character.
-   To include a `-', you must use it in a context where it cannot possibly
-   indicate a range: that is, as the first character, or immediately after
-   a range.
-  @item{|} Specifies an alternative.  Two @var{regexp_shell} A and B with
-   | in between form an expression that matches anything that either A or B
-   will match.
-  @item{@{...@}} Groups alternatives inside larger @var{regexp_shell}.
-  @item{\\} Quotes a special character (including itself).
- @end{description}").
+
+  @item{@tt{*}} Matches any string that not begin with the character '@tt{.}'.
+
+  @item{@tt{?}} Matches any single character whenever it is not a starting point.
+
+  @item{@tt{[}...@tt{]}} Matches any one of the enclosed characters.
+     A pair of characters separated by a minus sign denotes a range;
+     any character lexically between those two characters, inclusive,
+     is matched.  If the first character following the @tt{[} is a
+     @tt{^} then any character not enclosed is matched.  No other
+     character is special inside this construct.  To include a @tt{]}
+     in a character set, you must make it the first character.  To
+     include a @tt{-}, you must use it in a context where it cannot
+     possibly indicate a range: that is, as the first character, or
+     immediately after a range.
+
+  @item{@tt{|}} Specifies an alternative.  Two regexps @tt{A} and
+     @tt{B} with @tt{|} in between form an expression that matches
+     anything that either @tt{A} or @tt{B} matches.
+
+  @item{@{...@}} Groups alternatives inside larger @em{regexp_shell}s.
+
+  @item{@tt{\\}} Quotes a special character (including itself).
+
+  @end{description}").
 
 
