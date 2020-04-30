2c77b1e00eca4cc9938b0bca4357e9bc935dc88a
Jose F. Morales <jfmcjf@gmail.com>
2020-04-30 11:35:55 +0200

    (core) Fixed library(use_url)
    Example:
    
    ?- use_module_url(hw, 'http://raw.githubusercontent.com/ciao-lang/ciao/master/core/examples/hw.pl', [main/1]).
    
    yes
    ?- main(_).
    Hello world!
    
    Hit return to proceed...
    
    yes
    
    Src-commit: 0910c467a6299246620ca3f2ad18abfa34d47f34

--- core/library/use_url.pl
+++ core/library/use_url.pl
@@ -1,64 +1,55 @@
-:- module(use_url,
-    [use_module_url/2,use_module_url/3,use_module_url/4],
-    [assertions]).
+:- module(use_url, [
+    use_module_url/2,use_module_url/3,use_module_url/4
+], [assertions]).
 
-:- use_module(library(lists), [member/2]).
+:- use_module(library(pathnames), [path_concat/3]).
 :- use_module(library(compiler), [use_module/3]).
 :- use_module(library(system), 
     [delete_directory/1,delete_file/1,directory_files/2,
      make_directory/1,mktemp_in_tmp/2]). 
-:- use_module(engine(stream_basic)).
-:- use_module(engine(io_basic)).
+:- use_module(library(system_extra), [del_file_nofail/1]).
+:- use_module(engine(stream_basic), [sourcename/1]).
 
-:- use_module(library(http/http_client)).
-:- use_module(library(http/url)).
+:- use_module(library(http_get), [http_get/2]).
 
-:- pred use_module_url(Module, Url) : sourcename * string
+:- pred use_module_url(Module, URL) : sourcename * string
     # "Imports from module @var{Module} all the predicates exported 
       by it into the calling module. @var{Module} is loaded from
-      @var{Url}. If @var{Url} is not accesible, it fails.".
+      @var{URL}. If @var{URL} is not accesible, it fails.".
 
 :- meta_predicate use_module_url(?,addmodule).
 
-use_module_url(Mod,Url,This):- use_module_url(Mod,Url,all,This).
+use_module_url(Mod,URL,This):- use_module_url(Mod,URL,all,This).
 
-:- pred use_module_url(Module, Url, Imports) 
+:- pred use_module_url(Module, URL, Imports) 
     : sourcename * string * list(predname)
     # "Imports from module @var{Module} the predicates in @var{Imports}
       (if exported) into the calling module. @var{Module} is loaded from
-      @var{Url}. If @var{Url} is not accesible, it fails.".
+      @var{URL}. If @var{URL} is not accesible, it fails.".
 
 :- meta_predicate use_module_url(?,?,addmodule).
 
-use_module_url(Mod,NameUrl,Imports,This):-
-    url_info(NameUrl,Url),
-    fetch_url(Url,[],Response),
-    member(status(A,I,E),Response),
-    ( A = success
-    ; throw(use_url(status(A,I,E)))
-    ),
-    member(content(Content),Response),
+use_module_url(Mod, URL, Imports, This):-
     mktemp_in_tmp('ciaoXXXXXX',TmpDir),
-    atom_concat(TmpDir,Mod,TmpFile),
+    del_file_nofail(TmpDir),
+    path_concat(TmpDir,Mod,TmpFile),
     make_directory(TmpDir),
-    open(TmpFile,write,Stream),
-    putall(Content,Stream),
-    close(Stream),
+    %
+    http_get(URL, file(TmpFile)),
+    %
     use_module(TmpFile,Imports,This),
+    %
     ( directory_files(TmpDir,FileList),
       delete_all_files(FileList,Mod,TmpDir),
       delete_directory(TmpDir)
     ; true
     ).
 
-putall([],_Stream).
-putall([X|Xs],Stream):- put_code(Stream,X), putall(Xs,Stream).
-
 delete_all_files([],_F,_D).
 delete_all_files([File0|Files],TmpFile,TmpDir):-
-    ( atom_concat(TmpFile,_Any,File0)
-    -> atom_concat(TmpDir,File0,File),
-       delete_file(File)
-     ; true
+    ( atom_concat(TmpFile,_Any,File0) ->
+        path_concat(TmpDir,File0,File),
+        delete_file(File)
+    ; true
     ),
     delete_all_files(Files,TmpFile,TmpDir).
