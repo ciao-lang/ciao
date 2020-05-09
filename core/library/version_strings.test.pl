f39ce2996acca2da001153de4682fde72f6d02d4
Jose F. Morales <jfmcjf@gmail.com>
2020-05-09 10:20:54 +0200

    Minor
    Src-commit: 353bf4514bb33df818c2aab2315924806a3fa18b

--- core/library/version_strings.test.pl
+++ core/library/version_strings.test.pl
@@ -4,7 +4,7 @@
 
 :- use_module(library(version_strings)).
 
-:- export(test_norm/0). % export needed for test
+:- export(test_norm/0).
 :- test test_norm # "Comparison of normal versions".
 test_norm :- strict_sorted_vers(['1.0.0', '2.0.0', '2.1.0', '2.1.1']).
 
@@ -19,7 +19,6 @@ test_presub :- strict_sorted_vers([
     '1.0.0-beta', '1.0.0-beta.2', '1.0.0-beta.11',
     '1.0.0-rc.1', '1.0.0']).
 
-% TODO: only for tests! disable when not testing
 strict_sorted_vers([]).
 strict_sorted_vers([_]) :- !.
 strict_sorted_vers([A,B|Xs]) :- version_compare(<, A, B), strict_sorted_vers([B|Xs]).
