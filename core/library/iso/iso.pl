f02cb454f766a3750574fc0d667a1ddfa095e9d3
Jose F. Morales <jfmcjf@gmail.com>
2020-05-23 12:21:51 +0200

    (core) Improved ISO support (iso.pl package)
    Details:
    
     - More compatible version of absolute_file_name/2 (do not repeat last
       path component when resolving library paths).
    
     - Initial support for stream aliases (at this point only in open/4,
       close/1, close/2, stream_property/2 and for code using the ISO
       package).
    
       Different aliases may point to the same stream. Aliases are
       properly removed when streams are closed. Reusing an alias before
       it has been removed throws an error.
    
     - Import keysort/2 and format/? predicates.
    
     - Use runtime_ops package.
    
    Src-commit: b0ec39d7393c59a0d0c803639869001919e3b097

--- core/library/iso/iso.pl
+++ core/library/iso/iso.pl
@@ -8,7 +8,10 @@
 :- use_package(hiord). % TODO: refine, not all are ISO (required for call/N)
 :- use_module(engine(basic_props), [callable/1]).
 
+:- use_package(runtime_ops).
+
 :- use_module(library(aggregates)).
+:- use_module(library(sort), [sort/2, keysort/2]).
 :- use_module(library(iso_misc)).
 :- use_module(library(iso_char)).
 :- use_module(library(iso_incomplete)).
@@ -19,6 +22,32 @@
 :- use_module(library(terms_vars), [term_variables/2]).
 :- use_module(library(cyclic_terms), [acyclic_term/1]).
 
-:- use_module(engine(stream_basic)). % TODO: refine, not all are ISO
+:- use_module(library(format)).
+
+:- if(defined(optim_comp)).
+:- else.
+:- use_module(library(compiler)). % TODO: refine
+:- endif.
+:- use_module(library(system)). % TODO: refine
+
+:- use_module(engine(stream_basic), [
+    % TODO: refine, not all are ISO
+    open/3,
+    %open/4,
+    open_option_list/1,
+    %close/1,
+    set_input/1, current_input/1,
+    set_output/1, current_output/1,
+    character_count/2, line_count/2, line_position/2,
+    flush_output/1, flush_output/0,
+    %clearerr/1,
+    current_stream/3, stream_code/2,
+    %absolute_file_name/2,
+    absolute_file_name/7,
+    pipe/2,
+    sourcename/1, stream/1, stream_alias/1
+    %io_mode/1, 
+    %atm_or_int/1
+]).
 :- use_module(engine(io_basic)). % TODO: refine, not all are ISO
 :- use_module(engine(runtime_control)). % TODO: refine, not all are ISO
--- core/library/iso_incomplete.pl
+++ core/library/iso_incomplete.pl
@@ -1,76 +1,22 @@
-:- module(iso_incomplete,
-    [close/2,
-     close_options/1,
-     close_option/1,
-     stream_property/2,
-     stream_prop/1],
-     [assertions,isomodes]).
+:- module(iso_incomplete, [
+    absolute_file_name/2,
+    open/4,
+    close/1,
+    close/2,
+    close_options/1,
+    close_option/1,
+    stream_property/2,
+    stream_prop/1
+], [assertions,isomodes,datafacts]).
 
-:- doc(title, "Incomplete ISO Prolog predicates").
-
-:- doc(bug, "Merge with iso.pl and simply mark these predicates in the documentation as incomplete?"). 
+:- doc(title, "ISO Prolog compatibility layer").
 
 :- doc(author, "The Ciao Development Team").
 
 :- doc(module, "This module provides some additional ISO Prolog
    predicates whose implementation is not yet complete.").
 
-:- use_module(engine(stream_basic)).
-
-% :- entry open(+sourcename, +io_mode, ?stream, +open_option_list).
-
-% original:
-/*
-:- pred open(+sourcename, +io_mode, ?stream, +open_option_list).
-
-open(F, M, S, _) :- open(F, M, S).
-*/
-
-%:- entry close(+stream,+close_options). 
-:- pred close(@stream,@close_options). 
-
-:- doc(bug,"Options not completely implemented.").
-
-close(S, _) :- close(S).
-
-:- prop close_options(L) + regtype
-# "@var{L} is a list of @prop{close_option/1}.".
-
-close_options([]).
-close_options([O|Os]):-
-    close_option(O),
-    close_options(Os).
-
-:- prop close_option(O) + regtype 
-# "@var{O} is an option for close/2: @includedef{close_option/1}.".
-
-:- doc(bug, "Incomplete list of options."). 
-close_option(force(true)).
-close_option(force(false)).
-
-
-:- pred stream_property(?stream, ?stream_prop).
-
-stream_property(S, P) :- % It is not complete
-    current_stream(File, Mode, S),
-    ( P = file_name(File)
-    ; P = mode(Mode)
-    ; Mode = read ->
-        P = input
-    ; P = output
-    ).
-
-:- prop stream_prop(P) + regtype
-# "@var{P} is a valid stream property: @includedef{stream_prop/1}".  
-
-:- doc(bug, "stream_prop/1 is not complete wrt iso standard.").
-
-stream_prop(input).
-stream_prop(output).
-stream_prop(file_name(File)):-
-    atm(File).
-stream_prop(mode(Mode)):-
-    atm(Mode).
+:- doc(bug, "Introduce iso_incomplete/1 property? Or wait until we can merge them?"). 
 
 :- doc(bug, "at_end_of_stream :- not_yet_implemented.").
 :- doc(bug, "at_end_of_stream(_) :- not_yet_implemented.").
@@ -80,5 +26,132 @@ stream_prop(mode(Mode)):-
 :- doc(bug, "char_conversion(_,_) :- not_yet_implemented.").
 :- doc(bug, "current_char_conversion(_,_) :- not_yet_implemented.").
 
+:- use_module(engine(stream_basic)).
 
+%:- use_module(library(streams)).% TODO:debug
+
+% ---------------------------------------------------------------------------
+
+absolute_file_name(Path, ExpandedPath) :-
+    fixed_absolute_file_name(Path, '.', ExpandedPath).
+
+% ---------------------------------------------------------------------------
+
+% Stream aliases (one stream per alias)
+:- data '$alias_stream'/2.
+% Inverse index (from stream to aliases)
+:- data '$stream_alias'/2.
+
+% TODO: argument order changed!
+:- pred open(+sourcename, +io_mode, ?stream, +open_option_list).
+
+open(File, Mode, Stream, Opts) :-
+    get_aliases(Opts, OtherOpts, Aliases),
+    stream_basic:open(File, Mode, Stream, OtherOpts),
+    set_aliases(Aliases, Stream).
+
+% Get (and check) aliases options
+get_aliases([], [], []).
+get_aliases([Opt|Opts], OtherOpts, Aliases) :-
+    ( Opt = alias(Alias) ->
+        check_alias(Alias),
+        OtherOpts = OtherOpts0,
+        Aliases = [Alias|Aliases0]
+    ; OtherOpts = [Opt|OtherOpts0],
+      Aliases = Aliases0
+    ),
+    get_aliases(Opts, OtherOpts0, Aliases0).
+
+% Alias must be an atom and it cannot be reused
+check_alias(Alias) :-
+    ( atom(Alias) -> true
+    ; throw(error(type_error(atom, Alias), 'stream_basic:$open'/4-4))
+    ),
+    ( '$alias_stream'(Alias, _) ->
+        throw(error(permission_error(open, source_sink, alias(Alias)),
+                    'stream_basic:$open'/4-4))
+    ; true
+    ).
+
+set_aliases([], _).
+set_aliases([Alias|Aliases], Stream) :-
+    set_alias(Alias, Stream),
+    set_aliases(Aliases, Stream).
+
+set_alias(Alias, Stream) :-
+    % Remove alias 
+    ( retract_fact('$alias_stream'(Alias,S)),
+      retract_fact('$stream_alias'(S,Alias)),
+        fail
+    ; true
+    ),
+    % Assert (two indices)
+    asserta_fact('$alias_stream'(Alias, Stream)),
+    asserta_fact('$stream_alias'(Stream, Alias)).
+
+% Remove aliases associated to Stream
+remove_aliases(Stream) :-
+    ( retract_fact('$stream_alias'(Stream, Alias)),
+      retract_fact('$alias_stream'(Alias, _)),
+        fail
+    ; true
+    ).
+
+% alias_stream(?Alias,?Stream): Stream is the stream associated to Alias
+alias_stream(Alias, Stream) :- nonvar(Alias), !,
+    '$alias_stream'(Alias, Stream).
+alias_stream(Alias, Stream) :- % (nondet)
+    '$stream_alias'(Stream, Alias).
+
+resolve_stream_alias(Alias, Stream) :- atom(Alias),
+    '$alias_stream'(Alias, Stream0), !, Stream = Stream0.
+resolve_stream_alias(Stream, Stream).
+
+:- pred close(@stream,@close_options). 
+
+:- doc(bug, "close/2 not complete w.r.t. iso standard.").
+
+close(S, _) :-
+    resolve_stream_alias(S, S2),
+    ( nonvar(S2) -> remove_aliases(S2) ; true ),
+    stream_basic:close(S2).
+
+:- prop close_options(L) + regtype
+   # "@var{L} is a list of @prop{close_option/1}.".
+
+close_options([]).
+close_options([O|Os]):-
+    close_option(O),
+    close_options(Os).
+
+:- prop close_option(O) + regtype 
+   # "@var{O} is an option for close/2: @includedef{close_option/1}.".
+
+close_option(force(true)).
+close_option(force(false)).
+
+:- pred close(@stream). 
+close(S) :- close(S, []).
+
+:- pred stream_property(?stream, ?stream_prop).
+
+:- doc(bug, "stream_property/2 not complete w.r.t. iso standard.").
+
+stream_property(S, P) :-
+    current_stream(File, Mode, S),
+    ( P = file_name(File)
+    ; P = mode(Mode)
+    ; P = alias(Alias), alias_stream(Alias, S)
+    ; % (last case)
+      ( Mode = read -> P = input ; P = output )
+    ).
+
+:- prop stream_prop(P) + regtype
+   # "@var{P} is a valid stream property: @includedef{stream_prop/1}".  
+
+stream_prop(input).
+stream_prop(output).
+stream_prop(file_name(File)) :- atm(File).
+stream_prop(mode(Mode)) :- atm(Mode).
+stream_prop(alias(Alias)) :- atm(Alias).
 
