80971d3460c972db9bddce79b923612fe145bb16
Jose F. Morales <jfmcjf@gmail.com>
2020-06-02 10:01:12 +0200

    (core) Show note message for modules compiled with the trace or debug packages
    Src-commit: df66a3e64f5b916b52d5b19ee63b5e8dd2e81cd7

--- core/lib/debug.pl
+++ core/lib/debug.pl
@@ -1,6 +1,8 @@
 :- package(debug).
 % Enable embedded debugger in 'debug' mode (for the module that uses this package)
 
+?- error_in_lns(_,_,note, 'Using debug package').
+
 :- use_package(library(debugger/embedded)).
 % TODO: Not very nice, avoid this use of initialization!
 :- initialization((this_module(M), debug_module(M), debug)).
--- core/lib/trace.pl
+++ core/lib/trace.pl
@@ -1,6 +1,8 @@
 :- package(trace).
 % Enable embedded debugger in 'trace' mode (for the module that uses this package)
 
+?- error_in_lns(_,_,note, 'Using trace package').
+
 :- use_package(library(debugger/embedded)).
 % TODO: Not very nice, avoid this use of initialization!
 :- initialization((this_module(M), debug_module(M), trace)).
