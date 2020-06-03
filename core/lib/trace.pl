e67c79b07b39346e766813d61173c48762dd4984
Jose F. Morales <jfmcjf@gmail.com>
2020-06-03 19:09:03 +0200

    (core) debug code (commented out by default) in trace.pl to improve src location
    Src-commit: 17255a8e9912801cc51e22d29075d1abc98a81d7

--- core/lib/trace.pl
+++ core/lib/trace.pl
@@ -1,8 +1,10 @@
 :- package(trace).
 % Enable embedded debugger in 'trace' mode (for the module that uses this package)
 
+%?- ((location(Src,Ln0,Ln1),error_in_lns(Ln0,Ln1,note, ['(at ', Src, ')']),fail;true),
+%    error_in_lns(_,_,note, 'Using trace package')).
 ?- error_in_lns(_,_,note, 'Using trace package').
-
+    
 :- use_package(library(debugger/embedded)).
 % TODO: Not very nice, avoid this use of initialization!
 :- initialization((this_module(M), debug_module(M), trace)).
