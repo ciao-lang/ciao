90005a5e147f5ef7079e2fa3d687744e6535de23
Jose F. Morales <jfmcjf@gmail.com>
2020-10-07 11:06:07 +0200

    (engine) fix, adding back prototypes for working with attributes from C in attributes.h
    Src-commit: 4b027bc9b4e8f2319210892712c98a8df962ccd9

--- core/engine/attributes.h
+++ core/engine/attributes.h
@@ -7,6 +7,12 @@
 #ifndef _CIAO_ATTRIBUTES_H
 #define _CIAO_ATTRIBUTES_H
 
+/* TODO: needed here because of tclp libs, provide a better interface? */
+CBOOL__PROTO(bu1_detach_attribute, tagged_t x);
+CBOOL__PROTO(bu2_attach_attribute, tagged_t var, tagged_t constr);
+CBOOL__PROTO(bu2_update_attribute, tagged_t x, tagged_t constr);
+CFUN__PROTO(fu1_get_attribute, tagged_t, tagged_t x);
+
 CVOID__PROTO(collect_one_pending_unification);
 CVOID__PROTO(collect_pending_unifications, intmach_t wake_count);
 
