61b21627586c08cf00f31de240002604570c0604
vic <victor.pc.upm@gmail.com>
2021-01-30 12:46:22 +0100

    (ciaopp_cost) enhancing documentation of properties and directives
    This commit adds/improves/fixes the following documentation:
    
     - directives at resdefs(resources_decl)
     - properties at resdefs(resources_props)
     - regular types at resdefs(resources_types)
     - properties at native_props_cost.pl (in the 'core' bundle)
    
    It also separates more cleanly separated the documentation from the
    resources_decl.pl package into a separate resources_decl_doc.pl file
    (as needed due to current technical limitations in ciao and lpdoc).
    
    Also, internal bound/1 was moved from to native_props_cost.pl to
    resdefs(resources_types), and approx/1 was moved from
    resdefs(resources_types) to native_props_cost.pl .
    
    Src-commit: ffea807614a7736f0405aa24316506611bf90c93

--- core/lib/assertions/native_props_cost.pl
+++ core/lib/assertions/native_props_cost.pl
@@ -1,33 +1,264 @@
 % (included file)
 :- doc(section, "Data sizes, cost, termination").
 
-:- doc(bug, "Should probably find a better name...").
-
 :- if(defined(optim_comp)).
 :- else.
-:- export(bound/1).
-:- doc(bound/1,"The types approximation (bounding) supported in size
-   and cost analysis (see also @lib{resources_basic.pl}).
+% TODO: @item @tt{me} (unsupported): The mean of @tt{ub} and @tt{lb}.
+% TODO: @item @tt{ome} (unsupported): Complexity order of @tt{me}.
+% TODO: @item @tt{olb} (supported in @lib{infercost} analysis): Omega.
+% TODO: @item @tt{oub} (supported in @lib{infercost} analysis): Big O.
+:- export(approx/1).
+:- doc(approx/1,"The types of approximations (bounding) supported by the size
+   and cost analyses (see also @lib{resources_basic.pl}).
+
    @begin{itemize}
-   @item @tt{upper}: an upper bound.
-   @item @tt{lower}: a lower bound.
+   @item @tt{ub}: for upper bounds.
+   @item @tt{lb}: for lower bounds.
+   @item @tt{exact}: for an exact expression.
+   @item @tt{o}: for the big-O.
    @end{itemize}
 ").
-:- regtype bound(X) # "@var{X} is a direction of approximation (upper
-   or lower bound).".
-
-bound(upper).
-bound(lower).
+:- regtype approx(X) # "@var{X} represents an approximation.".
+approx(ub).
+approx(lb).
+approx(me).
+approx(oub).
+approx(olb).
+approx(o).
+approx(ome).
+approx(exact).
 :- endif.
 
 % --------------------------------------------------------------------------
 
+:- if(defined(optim_comp)).
+:- else.
+:- export(resource_id/1).
+
+:- doc(resource_id/1,"A resource is a numerical property that varies
+   (is consumed) throughout the execution of a piece of
+   code. Resources can be predefined (i.e., in a library) or
+   user-defined.  Examples are computational steps, time spent, memory
+   usage, bytes sent over a wire, database operations, energy
+   consumed, etc.").
+:- regtype resource_id(X)
+   # "@var{X} is the name of a resource (see @lib{resources_decl}).".
+resource_id(T) :- atm(T).
+:- endif.
+
+% --------------------------------------------------------------------------
+
+:- if(defined(optim_comp)).
+:- else.
+:- export(cost_expression/1).
+
+:- doc(cost_expression/1,"A cost expression is a symbolic function
+   representing the cost of executing a piece of code in terms of the
+   size of its input arguments and possibly other parameters. It is a
+   term built from elements of the lattice of real numbers (see
+   @pred{number_lattice}), numerical constants (see
+   @pred{numeric_constant/1}), size metrics (see @pred{size_metric})
+   and the following functors:
+
+   @begin{itemize}
+
+   @item @pred{- /1}: sign reversal.
+
+   @item @pred{+ /1}: identity.
+
+   @item @pred{-- /1}: decrement by one.
+
+   @item @pred{++ /1}: increment by one.
+
+   @item @pred{+ /2}: addition.
+
+   @item @pred{- /2}: subtraction.
+
+   @item @pred{* /2}: multiplication.
+
+   @item @pred{/ /2}: division.
+
+   @item @pred{** /2}: exponentiation.
+
+   @item @pred{exp/2}: exponentiation (DEPRECATED use @pred{** /2}).
+
+   @item @pred{log/2}: logarithm given the base.
+
+   @item @pred{log10/1}: 10-base logarithm.
+
+   @item @pred{log2/1}: 2-base logarithm.
+
+   @item @pred{exp/1}: exponential (@em{e} to the power of).
+
+   @item @pred{log/1}: natural logarithm (base @em{e}).
+
+   @item @pred{sqrt/1}: square root.
+
+   @item @pred{fact/1}: factorial.
+
+   @item @pred{max/2}: maximum.
+
+   @item @pred{min/2}: minimum.
+
+   @item @pred{sum(Index,LowerBound,UpperBound,Exp)}: summation of
+   @var{Exp} from @var{LowerBound} to @var{UpperBound} given the index
+   variable @var{Index}.
+
+   @item @pred{prod(Index,LowerBound,UpperBound,Exp)}: product of
+   @var{Exp} from @var{LowerBound} to @var{UpperBound} given the index
+   variable @var{Index}.
+
+   @end{itemize}
+").
+:- regtype cost_expression(X) # "@var{X} is a cost expression.".
+cost_expression(X) :- numeric_constant(X).
+cost_expression(X) :- size_term(X).
+cost_expression(X) :- number_lattice(X).
+cost_expression(+X) :- cost_expression(X).
+cost_expression(-X) :- cost_expression(X).
+cost_expression(++X) :- cost_expression(X).
+cost_expression(--X) :- cost_expression(X).
+cost_expression(X+Y) :- cost_expression(X), cost_expression(Y).
+cost_expression(X-Y) :- cost_expression(X), cost_expression(Y).
+cost_expression(X*Y) :- cost_expression(X), cost_expression(Y).
+cost_expression(X/Y) :- cost_expression(X), cost_expression(Y).
+cost_expression(X**Y) :- cost_expression(X), cost_expression(Y).
+cost_expression(exp(X)) :- cost_expression(X).
+cost_expression(log(X,Y)) :- cost_expression(X), cost_expression(Y).
+cost_expression(log(X)) :- cost_expression(X).
+cost_expression(log2(X)) :- cost_expression(X).
+cost_expression(log10(X)) :- cost_expression(X).
+cost_expression(sqrt(X)) :- cost_expression(X).
+cost_expression(fact(X)) :- cost_expression(X).
+cost_expression(max(X,Y)) :- cost_expression(X), cost_expression(Y).
+cost_expression(min(X,Y)) :- cost_expression(X), cost_expression(Y).
+cost_expression(sum(Index,LowerBound,UpperBound,Expr)) :-
+    indexvar(Index),
+    cost_expression(LowerBound),
+    cost_expression(UpperBound),
+    agg_expression(Expr).
+cost_expression(prod(Index,LowerBound,UpperBound,Expr)) :-
+    indexvar(Index),
+    cost_expression(LowerBound),
+    cost_expression(UpperBound),
+    agg_expression(Expr).
+cost_expression(exp(X,Y)) :- cost_expression(X), cost_expression(Y).
+:- endif.
+% --------------------------------------------------------------------------
+
+:- if(defined(optim_comp)).
+:- else.
+:- export(agg_expression/1).
+:- doc(agg_expression/1,"An aggregation expression is an expression
+   appearing in an aggregation function (@pred{sum/4},
+   @pred{prod/4}). An aggregation expression can be a regular cost
+   expression (see @pred{cost_expression/1}) or it may include index
+   variables as well (see @pred{indexvar/1}).
+").
+:- regtype agg_expression(X)
+   # "@var{X} is an aggregation expression.".
+agg_expression(X) :- numeric_constant(X).
+agg_expression(X) :- indexvar(X).
+agg_expression(X) :- size_term(X).
+agg_expression(X) :- number_lattice(X).
+agg_expression(+X) :- agg_expression(X).
+agg_expression(-X) :- agg_expression(X).
+agg_expression(++X) :- agg_expression(X).
+agg_expression(--X) :- agg_expression(X).
+agg_expression(X+Y) :- agg_expression(X), agg_expression(Y).
+agg_expression(X-Y) :- agg_expression(X), agg_expression(Y).
+agg_expression(X*Y) :- agg_expression(X), agg_expression(Y).
+agg_expression(X/Y) :- agg_expression(X), agg_expression(Y).
+agg_expression(X**Y) :- agg_expression(X), agg_expression(Y).
+agg_expression(exp(X)) :- agg_expression(X).
+agg_expression(log(X,Y)) :- agg_expression(X), agg_expression(Y).
+agg_expression(log(X)) :- agg_expression(X).
+agg_expression(log2(X)) :- agg_expression(X).
+agg_expression(log10(X)) :- agg_expression(X).
+agg_expression(sqrt(X)) :- agg_expression(X).
+agg_expression(fact(X)) :- agg_expression(X).
+agg_expression(max(X,Y)) :- agg_expression(X), agg_expression(Y).
+agg_expression(min(X,Y)) :- agg_expression(X), agg_expression(Y).
+agg_expression(sum(Index,LowerBound,UpperBound,Expr)) :-
+    indexvar(Index),
+    agg_expression(LowerBound),
+    agg_expression(UpperBound),
+    agg_expression(Expr).
+agg_expression(prod(Index,LowerBound,UpperBound,Expr)) :-
+    indexvar(Index),
+    agg_expression(LowerBound),
+    agg_expression(UpperBound),
+    agg_expression(Expr).
+agg_expression(exp(X,Y)) :- agg_expression(X), agg_expression(Y).
+:- endif.
+
+% --------------------------------------------------------------------------
+
+:- if(defined(optim_comp)).
+:- else.
+:- export(numeric_constant/1).
+:- doc(numeric_constant/1,"A numeric constant is a fixed, well-defined
+   real number.
+").
+:- regtype numeric_constant(X) # "@var{X} represents a numeric constant.".
+numeric_constant(e).
+:- endif.
+% --------------------------------------------------------------------------
+
+:- if(defined(optim_comp)).
+:- else.
+:- export(size_term/1).
+:- doc(size_term/1,"A size term is a term in a cost expression (see
+   @pred{cost_expression/1}) representing the size of a term in a given
+   metric (see @pred{measure_t/1}).
+").
+:- regtype size_term(X) # "@var{X} represents the size of a term.".
+size_term(int(X)) :- term(X).
+size_term(length(X)) :- term(X).
+size_term(size(X)) :- term(X).
+size_term(depth(X)) :- term(X).
+:- endif.
+
+% --------------------------------------------------------------------------
+
+:- if(defined(optim_comp)).
+:- else.
+:- export(indexvar/1).
+:- doc(indexvar/1,"A variable used as an index in an aggregation
+   expression.
+").
+:- regtype indexvar(X)
+   # "@var{X} is an index variable.".
+indexvar($(X)) :- gnd(X).
+:- endif.
+
+% --------------------------------------------------------------------------
+
+:- if(defined(optim_comp)).
+:- else.
+:- export(number_lattice/1).
+:- doc(number_lattice/1,"A lattice containing real numbers, @tt{inf} and @tt{bot}.
+").
+:- regtype number_lattice(X) # "@var{X} is a number, @tt{inf} or @tt{bot}.".
+number_lattice(inf).
+number_lattice(bot).
+number_lattice(X) :- number(X).
+:- endif.
+
+% --------------------------------------------------------------------------
 :- doc(bug,"size/3 vs. size_ub, size_lb redundant...").
 
 :- if(defined(optim_comp)).
 :- else.
 :- export(size/2).
-:- prop size(X, Y) + no_rtcheck
+:- doc(size(X,Y),"The exact size (for any approximation) of term
+   @var{X} is given by expression @var{Y}, which may depend on the
+   size of other terms. @tt{size(X,Y)} is equivalent to
+   @tt{size(exact,X,Y)}.
+").
+:- prop size(X, Y)
+   : ( term(X), cost_expression(Y) )
+   + no_rtcheck
    # "@var{Y} is the size of argument @var{X}, for any approximation.".
 
 size(_, _).
@@ -37,7 +268,13 @@ size(_, _).
 :- if(defined(optim_comp)).
 :- else.
 :- export(size/3).
-:- prop size(A, X, Y) : bound(A) + no_rtcheck
+:- doc(size(A,X,Y),"The size of term @var{X} for the approximation
+   @var{A} is given by expression @var{Y}, which may depend on the
+   size of other terms.
+").
+:- prop size(A, X, Y)
+   : ( approx(A), term(X), cost_expression(Y) )
+   + no_rtcheck
    # "@var{Y} is the size of argument @var{X}, for the approximation @var{A}.".
 
 size(_, _, _).
@@ -51,7 +288,9 @@ size(_, _, _).
    argument, e.g., list-length, term-size, term-depth, integer-value,
    etc. @cite{caslog,granularity-jsc}. See @pred{measure_t/1}.").
 
-:- prop size_lb(X, Y) + no_rtcheck
+:- prop size_lb(X, Y)
+   : ( term(X), cost_expression(Y) )
+   + no_rtcheck
    # "@var{Y} is a lower bound on the size of argument @var{X}.".
 
 :- if(defined(optim_comp)).
@@ -67,7 +306,9 @@ size(_, _, _).
    argument, e.g., list-length, term-size, term-depth, integer-value,
    etc. @cite{caslog,granularity-jsc}. See @pred{measure_t/1}.").
 
-:- prop size_ub(X, Y) + no_rtcheck
+:- prop size_ub(X, Y)
+   : ( term(X), cost_expression(Y) )
+   + no_rtcheck
    # "@var{Y} is a upper bound on the size of argument @var{X}.".
 
 :- if(defined(optim_comp)).
@@ -79,7 +320,9 @@ size(_, _, _).
 :- if(defined(optim_comp)).
 :- else.
 :- export(size_o/2).
-:- prop size_o(X, Y) + no_rtcheck
+:- prop size_o(X, Y)
+   : ( term(X), cost_expression(Y) )
+   + no_rtcheck
    # "The size of argument @var{X} is in the order of the expression
    @var{Y}.".
 
@@ -93,7 +336,8 @@ size(_, _, _).
 :- export(size_metric/3).
 :- meta_predicate size_metric(goal, ?, ?).
 :- prop size_metric(Head, Var, Metric)
-   :: measure_t(Metric) + no_rtcheck
+   : ( callable(Head), term(Var), measure_t(Metric) )
+   + no_rtcheck
    # "@var{Metric} is the measure used to determine the size of the
    terms that @var{Var} is bound to, for any type of approximation.".
 
@@ -105,7 +349,8 @@ size_metric(Goal, _, _) :- call(Goal).
 :- export(size_metric/4).
 :- meta_predicate size_metric(goal, ?, ?, ?).
 :- prop size_metric(Head, Approx, Var, Metric)
-   :: (bound(Approx), measure_t(Metric)) + no_rtcheck
+   : ( callable(Head), approx(Approx), term(Var), measure_t(Metric) )
+   + no_rtcheck
    # "@var{Metric} is the measure used to determine the size of the
    terms that variable @var{Var} bound to, for the approximation
    @var{Approx}.". % depth/1 See resources_basic
@@ -154,7 +399,9 @@ measure_t(depth([_|_])).
    @var{Y} @cite{low-bounds-ilps97,granularity-jsc}").
 
 :- meta_predicate steps_lb(goal, ?).
-:- prop steps_lb(X, Y) + no_rtcheck
+:- prop steps_lb(X, Y)
+   : ( callable(X), cost_expression(Y) )
+   + no_rtcheck
    # "@var{Y} is a lower bound on the cost of any call of the form @var{X}.".
 
 :- if(defined(optim_comp)).
@@ -169,7 +416,9 @@ steps_lb(Goal, _) :- call(Goal).
    @var{Y} @cite{caslog,granularity-jsc}.").
 
 :- meta_predicate steps_ub(goal, ?).
-:- prop steps_ub(X, Y) + no_rtcheck
+:- prop steps_ub(X, Y)
+   : ( callable(X), cost_expression(Y) )
+   + no_rtcheck
    # "@var{Y} is a upper bound on the cost of any call of the form @var{X}.".
 
 :- if(defined(optim_comp)).
@@ -183,7 +432,9 @@ steps_ub(Goal, _) :- call(Goal).
    any call of the form @var{X} is given by the expression @var{Y}").
 
 :- meta_predicate steps(goal, ?).
-:- prop steps(X, Y) + no_rtcheck
+:- prop steps(X, Y)
+   : ( callable(X), cost_expression(Y) )
+   + no_rtcheck
    # "@var{Y} is the cost (number of resolution steps) of any call of the
    form @var{X}.".
 
@@ -196,10 +447,16 @@ steps(Goal, _) :- call(Goal).
 :- if(defined(optim_comp)).
 :- else.
 :- export(steps_o/2).
+:- doc(steps_o(X, Y), "The big O expression of the computation (in
+   resolution steps) spent by any call of the form @var{X} is given by
+   the expression @var{Y}").
+
 :- meta_predicate steps_o(goal, ?).
-:- prop steps_o(X, Y) + no_rtcheck
-   # "@var{Y} is the complexity order (Big O) of the cost, given in
-     resolution steps, of any call of the form @var{X}.".
+:- prop steps_o(X, Y)
+   : ( callable(X), cost_expression(Y) )
+   + no_rtcheck
+   # "@var{Y} is the complexity order of the cost of any call of the form
+   @var{X}.".
 
 steps_o(Goal, _) :- call(Goal).
 :- endif.
@@ -208,6 +465,7 @@ steps_o(Goal, _) :- call(Goal).
 
 :- if(defined(optim_comp)).
 :- else.
+% TODO: Move rsize related regtypes here?
 :- export(rsize/2). % TODO:[new-resources]
 :- prop rsize(Var,SizeDescr) + no_rtcheck
    # "@var{Var} has its size defined by @var{SizeDescr}.".
@@ -219,8 +477,15 @@ steps_o(Goal, _) :- call(Goal).
 :- if(defined(optim_comp)).
 :- else.
 :- export(costb/4). % TODO:[new-resources]
-% TODO: missing meta_predicate
-:- prop costb(Goal,Resource,Lower,Upper) + no_rtcheck 
+:- doc(costb(Goal,Resource,Lower,Upper),"@var{Lower}
+   (res. @var{Upper}) is a (safe) lower (res. upper) bound on the cost
+   of the computation of @var{Goal} in terms of @var{Resource} units.
+").
+:- meta_predicate costb(goal, ?, ?, ?).
+:- prop costb(Goal,Resource,Lower,Upper)
+   : ( callable(Goal), resource_id(Resource), cost_expression(Lower),
+       cost_expression(Upper) )
+   + no_rtcheck
    # "@var{Lower} (resp. @var{Upper}) is a (safe) lower (resp. upper)
      bound on the cost of the computation of @var{Goal} expressed in
      terms of @var{Resource} units.".
@@ -229,11 +494,34 @@ steps_o(Goal, _) :- call(Goal).
 
 % --------------------------------------------------------------------------
 
+:- if(defined(optim_comp)).
+:- else.
+:- export(cost/4).
+:- doc(cost(Goal,Approx,Resource,Expr),"@var{Expr} is a (safe) upper
+   or lower bounds, depending on the value of @var{Approx} (see
+   @pred{approx/1}), of the cost of computation of the goal @var{Goal}
+   in terms of @var{Resource} units.
+").
+:- meta_predicate cost(goal, ?, ?, ?).
+:- prop cost(Goal,Approx,Resource,Expr)
+   : ( callable(Goal), approx(Approx), resource_id(Resource),
+       cost_expression(Expr) )
+   + no_rtcheck
+   # "@var{Expr} is a safe upper or lower bounds (depending on
+     @var{Approx}) of the cost of computing @var{Goal} in terms of
+     @var{Resource} units.".
+:- impl_defined(cost/4).
+:- endif.
+
+% --------------------------------------------------------------------------
+
 :- export(terminates/1).
 :- doc(terminates(X), "Calls of the form @var{X} always terminate.").
 
 :- meta_predicate terminates(goal).
-:- prop terminates(X) + no_rtcheck
+:- prop terminates(X)
+   : callable(X)
+   + no_rtcheck
    # "All calls of the form @var{X} terminate.".
 
 :- if(defined(optim_comp)).
