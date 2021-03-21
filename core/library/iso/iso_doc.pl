2287403929815c8ab0ffd73a1dd97317058c7b83
Jose F. Morales <jfmcjf@gmail.com>
2021-03-21 17:07:24 +0100

    (core) minor in iso_doc.pl
    Src-commit: 3f48905c0126b6111c7f639c06a4e1bc0701824a

--- core/library/iso/iso_doc.pl
+++ core/library/iso/iso_doc.pl
@@ -4,32 +4,32 @@
 
 :- doc(filetype,package).
 
-:- doc(title,"ISO-Prolog package").
+:- doc(title,"Stricter ISO-Prolog package").
 
 :- doc(author, "The Ciao Development Team").
 
-:- doc(module,"This package makes the standard @concept{ISO-Prolog}
-   predicates available by default in the modules that include it. In
-   particular, it includes the modules and packages listed in the
-   @em{Usage and interface} box below. Note that this package is also
-   included when loading the @ref{Classic Prolog} package (either
-   explicitly or implicity by using a @decl{module/2} declaration).
-   On the other hand, using this package by itself, i.e.: @tt{:-
-   module(..., ..., [iso]).}  @em{approximates} a strictly conforming
-   mode, i.e., a mode which rejects uses of non-ISO features (except
-   for those that are built-in in Ciao, such as the module system).
+:- doc(module,"This package enables stricter @concept{ISO-Prolog}
+   conformance by default in the modules that include it. In particular,
+   it includes the modules and packages listed in the @em{Usage and
+   interface} box below. Note that this package is also included when
+   loading the @ref{Classic Prolog} package (either explicitly or
+   implicity by using a @decl{module/2} declaration).  On the other
+   hand, using this package by itself, i.e.: @tt{:- module(..., ...,
+   [iso]).}  @em{approximates} a strictly conforming mode, i.e., a
+   mode which rejects uses of non-ISO features (except for those that
+   are built-in in Ciao, such as the module system).
 
    Also note that library predicates that correspond to those in the
    ISO-Prolog standard are marked accordingly throughout the manuals,
    and differences between the Ciao and the prescribed ISO-Prolog
    behaviours, if any, commented appropriately.
 
-   Compliance with ISO is still not complete: currently there are some
-   minor deviations in, e.g., the treatment of characters, the syntax,
-   some of the arithmetic functions, and part of the error system.  In
-   particular, the @decl{char_conversion/2} directive is not
-   implemented, since Ciao does not (yet) have a character conversion
-   table.
+   Strict compliance with ISO is still not complete: currently there
+   are some minor deviations in, e.g., the treatment of characters,
+   the syntax, some of the arithmetic functions, and part of the error
+   system.  In particular, the @decl{char_conversion/2} directive is
+   not implemented, since Ciao does not (yet) have a character
+   conversion table.
 
    However, as mentioned in @ref{ISO-Prolog compliance versus
    extensibility} in the introduction, the intention of the Ciao
