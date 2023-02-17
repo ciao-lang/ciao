feab647e92899d8eefaa4926eb49d6634c7fad1c
Jose F. Morales <jfmcjf@gmail.com>
2023-02-17 04:36:49 +0100

    (engine) merged system_info.c
    Src-commit: 8d32cd1a1f83870fb8ab9d1528090e9307054370

--- core/engine/system_info.c
+++ core/engine/system_info.c
@@ -11,61 +11,65 @@
 #include <ciao/eng_registry.h>
 #endif
 
-extern char *eng_architecture;
-
-/* get_arch(?ArchName) */
-CBOOL__PROTO(prolog_getarch) {
-  CBOOL__LASTUNIFY(GET_ATOM(eng_architecture), X(0));
+#define DEF_PROLOG_GET_STR(NAME, VAR) \
+CBOOL__PROTO(prolog_##NAME) { \
+  CBOOL__LASTUNIFY(GET_ATOM(VAR), X(0)); \
 }
+
+#if defined(OPTIM_COMP)
+extern char *library_directory;
+DEF_PROLOG_GET_STR(ciao_lib_dir, library_directory);
+#endif
+
+extern char *eng_architecture;
+/* get_arch(?ArchName) */
+DEF_PROLOG_GET_STR(getarch, eng_architecture);
 
 extern char *eng_os;
-
 /* get_os(?OsName) */
-CBOOL__PROTO(prolog_getos) {
-  CBOOL__LASTUNIFY(GET_ATOM(eng_os), X(0));
-}
+DEF_PROLOG_GET_STR(getos, eng_os);
 
+#if !defined(OPTIM_COMP)
 extern char *eng_debug_level;
-
 /* eng_debug_level(?DebugLevel) */
-CBOOL__PROTO(prolog_eng_debug_level) {
-  CBOOL__LASTUNIFY(GET_ATOM(eng_debug_level), X(0));
-}
+DEF_PROLOG_GET_STR(eng_debug_level, eng_debug_level);
+#endif
 
+#if !defined(OPTIM_COMP)
 extern int eng_is_sharedlib;
-
 CBOOL__PROTO(prolog_eng_is_sharedlib) {
   return eng_is_sharedlib;
 }
+#endif
 
+#if !defined(OPTIM_COMP)
 extern char *ciao_suffix;
 extern char *exec_suffix;
 extern char *so_suffix;
-
-CBOOL__PROTO(prolog_get_ciao_ext) {
-  CBOOL__LASTUNIFY(GET_ATOM(ciao_suffix), X(0));
-}
-
-CBOOL__PROTO(prolog_get_exec_ext) {
-  CBOOL__LASTUNIFY(GET_ATOM(exec_suffix), X(0));
-}
-
-CBOOL__PROTO(prolog_get_so_ext) {
-  CBOOL__LASTUNIFY(GET_ATOM(so_suffix), X(0));
-}
+DEF_PROLOG_GET_STR(get_ciao_ext, ciao_suffix);
+DEF_PROLOG_GET_STR(get_exec_ext, exec_suffix);
+DEF_PROLOG_GET_STR(get_so_ext, so_suffix);
+#endif
 
 extern char *foreign_opts_cc;
 extern char *foreign_opts_ld;
 extern char *foreign_opts_ccshared;
 extern char *foreign_opts_ldshared;
+#if defined(OPTIM_COMP)
+DEF_PROLOG_GET_STR(get_so_cc, foreign_opts_cc);
+DEF_PROLOG_GET_STR(get_so_ld, foreign_opts_ld);
+DEF_PROLOG_GET_STR(get_so_cc_opts, foreign_opts_ccshared);
+DEF_PROLOG_GET_STR(get_so_ld_opts, foreign_opts_ldshared);
+#else
+DEF_PROLOG_GET_STR(get_foreign_opts_cc, foreign_opts_cc);
+DEF_PROLOG_GET_STR(get_foreign_opts_ld, foreign_opts_ld);
+DEF_PROLOG_GET_STR(get_foreign_opts_ccshared, foreign_opts_ccshared);
+DEF_PROLOG_GET_STR(get_foreign_opts_ldshared, foreign_opts_ldshared);
+#endif
 
-#define DEF_PROLOG_GET_STR(NAME) \
-CBOOL__PROTO(prolog_get_##NAME) { \
-  CBOOL__LASTUNIFY(GET_ATOM(NAME), X(0)); \
-}
-
-DEF_PROLOG_GET_STR(foreign_opts_cc);
-DEF_PROLOG_GET_STR(foreign_opts_ld);
-DEF_PROLOG_GET_STR(foreign_opts_ccshared);
-DEF_PROLOG_GET_STR(foreign_opts_ldshared);
-
+#if defined(OPTIM_COMP)
+// extern char *emulator__so_libs; // TODO: deprecate
+// DEF_PROLOG_GET_STR(get_so_libs, emulator__so_libs);
+DEF_PROLOG_GET_STR(get_so_libs, "");
+DEF_PROLOG_GET_STR(get_exec_ext, "");
+#endif
