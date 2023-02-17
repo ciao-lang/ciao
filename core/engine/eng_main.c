92007f5e0b29eb94cbe0410e2f1dcea32e602af0
Jose F. Morales <jfmcjf@gmail.com>
2023-02-17 04:15:40 +0100

    (engine) merged eng_main.c
    Src-commit: 0ba86f39d8d853df0a57100a9404223a6099254e

--- core/engine/eng_main.c
+++ core/engine/eng_main.c
@@ -9,6 +9,12 @@
  *  Copyright (C) 2020 The Ciao Development Team
  */
 
+#include <ciao/eng.h>
+#if !defined(OPTIM_COMP)
+#include <ciao/eng_start.h>
+#endif
+
+#if !defined(OPTIM_COMP)
 #include <stdlib.h> /* atoi() */
 
 /* String that holds the size of the engine executable stub. The last
@@ -16,13 +22,13 @@
 char eng_stub_length_holder[] = "This emulator executable has a size of        ";
 
 void eng_stub_set_length(int length);
-int engine_start(int argc, char **argv);
-void engine_exit(int exit_code);
+#endif
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+#if !defined(OPTIM_COMP)
   int len = atoi(&eng_stub_length_holder[38]);
   eng_stub_set_length(len);
+#endif
   engine_exit(engine_start(argc, argv));
   return 0;
 }
