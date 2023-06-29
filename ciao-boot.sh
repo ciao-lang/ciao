be473752cac1d22a83cd3fe6299f8433a066a975
Jose F. Morales <jfmcjf@gmail.com>
2023-06-29 13:05:50 +0200

    Updated release tag
    Src-commit: 9e9cbfa75554d8c6ecc71353202aa6efdc28829b

--- ciao-boot.sh
+++ ciao-boot.sh
@@ -40,8 +40,8 @@ fi
 
 release_query_info() {
     case "$v__release" in
-        stable) tag=v1.22.0-m4 ;;
-        beta)   tag=v1.22.0-m4 ;;
+        stable) tag=v1.22.0-m5 ;;
+        beta)   tag=v1.22.0-m5 ;;
         latest) tag=master ;;
     esac
     # Set other defaults based on release
