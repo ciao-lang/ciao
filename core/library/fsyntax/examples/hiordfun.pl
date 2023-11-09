91ca406ff07e2dec6bd4d4503af921d81704e3c7
Jose F. Morales <jfmcjf@gmail.com>
2023-11-09 19:11:36 +0100

    (core) updated fsyntax/examples/hiordfun.pl
    Src-commit: e9cef7f8f91cc6d021646c0d7fac6fdd91eaa652

--- core/library/fsyntax/examples/hiordfun.pl
+++ core/library/fsyntax/examples/hiordfun.pl
@@ -1,24 +1,24 @@
-:- module(hiordfun, _, [functional,hiord,assertions,regtypes,isomodes]).
+:- module(hiordfun, _, [fsyntax,hiord,assertions,regtypes,isomodes]).
 
-:- doc(title, "Test for hiord and functional package").
-:- doc(author, "Jose F. Morales").
-
-:- doc(module, "Testing hiord and functions").
+%! \title Tests for hiord and fsyntax
+%  \author Jose F. Morales
+%
+%  \module Testing hiord and functions
 
 :- use_module(engine(io_basic)).
 
+:- fun_eval arith(true).
 :- fun_eval hiord(true).
 
 test :-
     T = 1 + 3,
     display(begin(T)), nl,
-    P = (Inc -> ''(A, R) :- R = Inc+A),
+    P = { Inc -> ''(A, R) :- R = Inc+A },
     Inc = 1000,
-    P2 = (''(A) := R :- R = 100+A),
-    P3 = (''(A) := 10 + A),
+    P2 = { ''(A) := R :- R = 100+A },
+    P3 = { ''(A) := 10 + A },
     display(p(P)), nl,
     display(p2(P2)), nl,
     display(p3(P3)), nl,
     X = ~P(~P2(~P3(1))),
     display(X), nl. % it should say 1111
-
