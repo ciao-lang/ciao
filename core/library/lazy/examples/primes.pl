3c729d0387a3f21bc65df3c717e9ef7647beb43b
Jose F. Morales <jfmcjf@gmail.com>
2024-02-02 15:59:51 +0100

    (core) fix lazy primes example
    Src-commit: f8deaea0baee557d04e18eb850c24944d6a6bd17

--- core/library/lazy/examples/primes.pl
+++ core/library/lazy/examples/primes.pl
@@ -2,22 +2,21 @@
 
 :- use_module(library(lazy/lazy_lib), [nums_from/2, take/3]).
 
-
 :- lazy cut/2.
 cut([], []).
-cut([H | T], [H | T2]) :-
-    cut_(H, T, S),
+cut([H | T], R) :-
+    cut_(T, H, S),
     !,
-    cut(S, T2).
+    cut(S, T2),
+    R = [H | T2].
 
 :- lazy cut_/3.
-cut_(_, [], []) :- !.
-cut_(H1, [H2 | T], [H2 | T2]) :-
-    cut_(H1, T, T2),
-    H2 mod H1 > 0.
-cut_(H1, [H2 | T], L) :-
-    cut_(H1, T, L),
-    H2 mod H1 =:= 0.
+cut_([], _, []) :- !.
+cut_([H2 | T], H1, R) :-
+    cut_(T, H1, T2),
+    ( H2 mod H1 > 0 -> R = [H2 | T2]
+    ; R = T2
+    ).
 
 :- lazy primes/1.
 primes(X) :-
--- core/library/lazy/examples/primes_function.pl
+++ core/library/lazy/examples/primes_function.pl
@@ -4,14 +4,15 @@
 
 :- lazy fun_eval cut/1.
 cut([])      := [].
-cut([H | T]) := [H | T2] :- cut_(H, T, S), !, cut(S, T2).
+cut([H | T]) := [H | ~cut(~cut_(T, H))].
 
 :- lazy fun_eval cut_/2.
-cut_(_, [])        := [].
-cut_(H1, [H2 | T]) := [H2 | T2] :- cut_(H1, T, T2), H2 mod H1  >  0.
-cut_(H1, [H2 | T]) := L         :- cut_(H1, T, L),  H2 mod H1 =:= 0.
+cut_([], _)        := [].
+%cut_([H2 | T], H1) := ( H2 mod H1 > 0 ? [H2 | T2] | T2 ) :- cut_(T, H1, T2).
+cut_([H2 | T], H1) := R :-
+    R = ( H2 mod H1 > 0 ? [H2 | ~cut_(T, H1)] | ~cut_(T, H1) ).
 
-% :- lazy fun_eval primes/0.
-% primes := ~cut(~nums_from(2)).
+:- lazy fun_eval primes/0.
+primes := ~cut(~nums_from(2)).
 
-test_primes(N) := ~take(N, ~cut(~nums_from(2))).
+test_primes(N) := ~take(N, ~primes).
