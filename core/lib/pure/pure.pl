739074c9c4b0ab09bed491e88030ef52b45270af
Jose F. Morales <jfmcjf@gmail.com>
2024-05-07 21:20:29 +0200

    (core) extending predicates allowed with the pure package
     - imported ;/2, =/2, false/0
     - imported srcdbg_spy/7 (internal, required for debugger)
    
    Src-commit: 4156767d23a10c56572297e150bfc644a8ee586c

--- core/lib/pure/pure.pl
+++ core/lib/pure/pure.pl
@@ -1,6 +1,17 @@
 :- package(pure).
 :- use_package(initial).
 
-:- use_module(engine(basiccontrol),[','/2, fail/0, true/0]).
+:- use_module(engine(term_basic), [(=)/2]).
+:- use_module(engine(basiccontrol), [
+    ','/2,
+    ';'/2,
+    fail/0,
+    true/0,
+    false/0
+]).
+:- if(defined(optim_comp)).
+:- else.
+:- use_module(engine(debugger_support), [srcdbg_spy/7]). % TODO: internal, make it optional or hide
+:- endif.
 
 %:- set_prolog_flag(unknown, fail).
