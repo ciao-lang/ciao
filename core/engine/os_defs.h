84a4cdf4e34e84cf1a9bba0bd665beb4064590a7
Jose F. Morales <jfmcjf@gmail.com>
2024-08-08 20:24:26 +0200

    adding missing file
    Src-commit: db5dc8d1d84d26fd561d45d6afd22e42faed22b2

---
+++ core/engine/os_defs.h
@@ -0,0 +1,24 @@
+/*
+ *  os_defs.h
+ *
+ *  Some OS definitions:
+ *   - MAXPATHLEN
+ *
+ *  See Copyright Notice in ciaoengine.pl
+ */
+
+#ifndef _CIAO_OS_DEFS_H
+#define _CIAO_OS_DEFS_H
+
+#include <limits.h>
+
+/* Max path name */
+#if !defined(MAXPATHLEN)
+# if defined(PATH_MAX)
+#  define MAXPATHLEN PATH_MAX
+# else
+#  define MAXPATHLEN 1024
+# endif
+#endif
+
+#endif /* _CIAO_OS_DEFS_H */
