4a43aee6b3c1c0df96b118f0888e9b186ff844aa
Manuel Hermenegildo <manuel.hermenegildo@imdea.org>
2024-10-01 17:54:18 +0200

    (core) Unified documentation of id and id_rh
    Src-commit: a0f5cfd8507961accb395dc0dc2ad5469c398bdf

--- core/library/id/id_doc.pl
+++ core/library/id/id_doc.pl
@@ -1,76 +1,76 @@
 :- use_package(assertions).
 :- doc(nodoc,assertions).
+:- doc(nodoc,assertions_basic).
 
 :- doc(title, "Iterative-deepening execution").
 
+:- doc(author, "R@'{e}my Haemmerl@'{e}").
+:- doc(author, "Manuel Carro").
 :- doc(author, "Claudio Vaucheret").
 :- doc(author, "Manuel Hermenegildo").
 
 :- doc(module,"This package applies a @em{compiling control} technique
-to implement @index{depth first iterative deepening} execution
+to implement @index{depth-first iterative deepening} execution
 @cite{iterative-deepening}. It changes the usual @em{depth-first}
-computation rule by @index{iterative-deepening} on those predicates
-specifically marked. This is very useful in search problems when a
+search rule by @index{iterative-deepening} on those predicates
+specifically marked. This is very useful in search problems as an
+alternative to, e.g., breadth-first search, when a
 @concept{complete proof procedure} is needed.
 
-When this computation rule is used, first all goals are expanded only up to
+When this search rule is used, first all goals are expanded only up to
 a given depth.  If no solution is found or more solutions are needed by
 backtracking, the depth limit is incremented and the whole goal is
 repeated.  Although it might seem that this approach is very inefficient
 because all higher levels are repeated for the deeper ones, it has been
 shown that is performs only about b/(b - 1) times as many operations than
-the corresponding breadth-first search, (where b is the branching factor of
-the proof tree) while the waste of memory is the same as depth first.
+the corresponding breadth-first search (where b is the branching factor of
+the proof tree), while the memory consumption is the same as depth first.
 
    The usage is by means of the following directive:
 
 @tt{:- iterative(Name, FirstCut, Formula).}
 
 which states than the predicate 'Name' given in functor/arity form will be
-executed using iterative deepening rule starting at the depth 'FirstCut'
-with depth being incremented by the predicate 'Formula'. This predicate
-compute the new depth using the previous one. It must implement a dilating
-function i.e. the new depth must be greater. For example, to start with
-depth 5 and increment by 10 you can write:
+executed using the iterative deepening rule, starting at depth @var{FirstCut}, 
+and with depth being incremented by predicate @var{Formula}. This predicate
+computes the new depth using the previous one. It must implement a dilating
+function i.e., the new depth must be greater than the old one. For example,
+to start with depth 5 and increment by 10 you can write:
 
 @tt{:- iterative(p/1,5,f).}
 
 @tt{f(X,Y) :- Y is X + 10.}
 
-or if you prefer,
+or, alternatively:
 
 @tt{:- iterative(p/1,5,(_(X,Y):- Y is X + 10)).}
 
 @cindex{depth limit}
-You can also use a fourth parameter to set a limiting depth. All goals below the given depth limit simply fail. Thus, with the following directive:
+You can also use a fourth parameter to set a limiting depth. Goals
+that reach the given depth limit simply fail. Thus, with the following
+directive:
 
 @tt{:- iterative(p/1,5,(_(X,Y):- Y is X + 10),100).}
 
 all goals deeper than 100 will fail. 
 
-An example of code using this package would be:
+This is a simple example using this package:
 
-@begin{verbatim}
-@includeverbatim{examples/example_id.pl}
-@end{verbatim}
+@includecode{id/examples/example_id.pl}
 
-The order of solutions are first the shallower and then the
-deeper. Solutions which are between two cutoff are given in the usual left
-to right order. For example,
+The order of solutions is, first the shallower ones and then the deeper
+ones. Solutions which are between two cutoffs are given in the usual
+left to right order. For example:
 
-@begin{verbatim}
-@includeverbatim{examples/example2.pl}
-@end{verbatim}
+@includecode{id/examples/example2.pl}
 
 It is possible to preserve the iterative-deepening behavior for calls to
 predicates defined in other modules. These modules should obviously also
-use this package. In addition @em{all} predicates from such modules should
-imported, i.e., the directive @tt{:- use_module(module)}, should be used in
+use this package. In addition, @em{all} predicates from such modules should
+be imported, i.e., the directive @tt{:- use_module(module)}, should be used in
 this case instead of @tt{:- use_module(module,[...])}.  Otherwise calls to
-predicates outside the module will be treated in the usual way i.e. by
+predicates outside the module will be treated in the usual way, i.e., by
 depth-first computation.
 
-Another complete proof procedure implemented is the @lib{bf} package
-(@concept{breadth first execution}).
-
-   ").
+Another complete proof procedure is implemented by the @lib{bf} package(s)
+(@concept{breadth first execution}).").
