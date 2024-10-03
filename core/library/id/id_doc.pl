95b02dbe9608af09cfa620de61deada9fb08db6a
Pedro Lopez Garcia <pedro.lopez@imdea.org>
2024-10-03 12:53:50 +0200

    (core) Minor tweaks in documentation.
    Src-commit: b8332ac5f126714b9c8a21f298fbc26690648e84

--- core/library/id/id_doc.pl
+++ core/library/id/id_doc.pl
@@ -30,12 +30,13 @@ the proof tree), while the memory consumption is the same as depth first.
 
 @tt{:- iterative(Name, FirstCut, Formula).}
 
-which states than the predicate 'Name' given in functor/arity form will be
-executed using the iterative deepening rule, starting at depth @var{FirstCut}, 
-and with depth being incremented by predicate @var{Formula}. This predicate
-computes the new depth using the previous one. It must implement a dilating
-function i.e., the new depth must be greater than the old one. For example,
-to start with depth 5 and increment by 10 you can write:
+which states than the predicate 'Name' given in functor/arity form
+will be executed using the iterative deepening search rule, starting
+at depth @var{FirstCut}, and with depth being incremented by predicate
+@var{Formula}. This predicate computes the new depth using the
+previous one. It must implement a dilating function i.e., the new
+depth must be greater than the old one. For example, to start with
+depth 5 and increment by 10 you can write:
 
 @tt{:- iterative(p/1,5,f).}
 
@@ -58,9 +59,11 @@ This is a simple example using this package:
 
 @includecode{id/examples/example_id.pl}
 
-The order of solutions is, first the shallower ones and then the deeper
-ones. Solutions which are between two cutoffs are given in the usual
-left to right order. For example:
+The order of solutions is as follows: first, the shallower ones and
+then the deeper ones in the resolution tree. Solutions between two
+cutoffs are given in the usual left-to-right order (i.e., the leftmost
+branch corresponds to the first unifying clause in the program, as in
+depth-first search). For example:
 
 @includecode{id/examples/example2.pl}
 
@@ -70,7 +73,7 @@ use this package. In addition, @em{all} predicates from such modules should
 be imported, i.e., the directive @tt{:- use_module(module)}, should be used in
 this case instead of @tt{:- use_module(module,[...])}.  Otherwise calls to
 predicates outside the module will be treated in the usual way, i.e., by
-depth-first computation.
+depth-first execution.
 
 Another complete proof procedure is implemented by the @lib{bf} package(s)
 (@concept{breadth first execution}).").
