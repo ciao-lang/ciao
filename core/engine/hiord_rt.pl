423b236b0ff18076f4f24cdac6c71cfa3ec49d4c
Jose F. Morales <jfmcjf@gmail.com>
2024-10-16 19:35:29 -0500

    (core) new internal $is_pa, $pa_env preds
    Src-commit: 3531ed107bd79f6ad8d268deaf8b4dd3b1d3b484

--- core/engine/hiord_rt.pl
+++ core/engine/hiord_rt.pl
@@ -1,13 +1,4 @@
-:- module(hiord_rt, 
-    [
-        call/1,
-        call/2 /* call/N, */, 
-        'SYSCALL'/1, 
-        '$nodebug_call'/1,
-        '$meta_call'/1,
-        '$meta_exp'/3,
-        this_module/1
-    ],[assertions, nortchecks, isomodes]).
+:- module(hiord_rt, [], [assertions, nortchecks, isomodes]).
 
 :- doc(title,"Higher-order support").
 
@@ -18,6 +9,8 @@
    implementation-defined predicate @pred{call/1}, and it implements
    the @pred{call/2} predicate.").
 
+% ---------------------------------------------------------------------------
+:- export(call/1).
 :- doc(call(G), "Executes goal @var{G}, restricting the scope of
    the cuts to the execution of @var{G}.  Equivalent to writing a
    variable @var{G} in a goal position.").
@@ -26,6 +19,8 @@
 :- meta_predicate call(primitive(goal)).
 :- impl_defined(call/1).
 
+% ---------------------------------------------------------------------------
+:- export(call/2). % call/N
 :- doc(call(Pred,Arg1), "The family of builtin predicates of the form
    @pred{call/N} with @tt{N > 1} execute the predicate @var{Pred}
    applying arguments @var{Arg1} ... @var{ArgN}. If @var{Pred} has
@@ -34,7 +29,6 @@
    using the @lib{hiord} package, the @tt{Pred(Arg1,...,ArgN)} syntax
    (with @var{Pred} a variable) is accepted as equivalent to
    @tt{call(Pred, Arg1, ..., ArgN)}.").
-
 
 % Won't get checked due to throw/1 in the first clause of calln/2.
 :- pred call(+cgoal,?term) + native. 
@@ -70,24 +64,51 @@ calln(Pred, Args) :-
     functor(Args,_,N),
     throw(error(type_error(pred(N),Pred), call/n-1)).
 
-
+% ---------------------------------------------------------------------------
+:- export('SYSCALL'/1).
 :- trust pred 'SYSCALL'(+cgoal).
 :- impl_defined('SYSCALL'/1).
 
-
+% ---------------------------------------------------------------------------
+:- export('$nodebug_call'/1).
 :- trust pred '$nodebug_call'(+cgoal).
 :- meta_predicate '$nodebug_call'(primitive(goal)).
 :- impl_defined('$nodebug_call'/1).
 
+% ---------------------------------------------------------------------------
+:- export('$meta_call'/1).
 :- trust pred '$meta_call'(+A) : cgoal(A) + native(call(A)).
 :- impl_defined('$meta_call'/1).
 
+% ---------------------------------------------------------------------------
+:- export('$meta_exp'/3).
 :- trust pred '$meta_exp'(+Metatype, +P, -E) # "Unify @var{E} with the
    metaexpansion of @var{P} with metatype @var{Metatype} (using static
    metaexpansion when possible and dynamic metaexpansion when
    necessary)".
 :- impl_defined('$meta_exp'/3).
 
+% ---------------------------------------------------------------------------
+:- export('$is_pa'/1).
+% PA is a predicate abstraction
+% TODO: define next to callable/1?
+'$is_pa'(PA) :-
+    nonvar(PA), PA = '$:'(PA1),
+    nonvar(PA1), PA1 = 'PAEnv'(_, _).
+
+:- export('$pa_env'/2).
+% Extract ShEnv shared environment from a PA:
+%  - captured variables for predicate abstractions
+%    (e.g., '$meta_exp'(goal, ([X,Y] -> ''(Z) :- append(X,Y,Z), _))) ;
+%  - or partial application (unqualified term)
+%    (e.g., '$meta_exp'(goal, append(X,Y,Z), _), '$meta_exp'(pred(2), append(X), _), etc.)
+% TODO: define next to callable/1?
+'$pa_env'(PA, ShEnv) :-
+    nonvar(PA), PA = '$:'(PA1),
+    nonvar(PA1), PA1 = 'PAEnv'(ShEnv, _).
+
+% ---------------------------------------------------------------------------
+:- export(this_module/1).
 :- meta_predicate this_module(addmodule).
 :- impl_defined(this_module/1). % TODO: avoid problems with addmodule and export
 
