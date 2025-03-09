2cc7cda518dddf038606d5d3519d2e96694b268d
Jose F. Morales <jfmcjf@gmail.com>
2025-03-09 19:47:41 +0100

    Ops, missing file in previous commit
    Src-commit: 1b6f1b9414b86aa7980156700b277f615340d8d5

---
+++ core/library/xsyntax/xsyntax.pl
@@ -0,0 +1,17 @@
+:- package(xsyntax).
+% Base translation for fsyntax and more
+
+:- if(defined(optim_comp)).
+% TODO: integrated, main incompatibilities and differences:
+%   - missing fsyntaxplus extensions
+%   - fun_return is not allowed
+%   - fun_eval does not allow fun_eval syntax
+%   - simpler translation for functional if-then-else (emit (_->_;_)
+%     directly, because it is correctly optimized by later
+%     optimizations)
+:- '$pragma'(functional_expand).
+:- else.
+:- load_compilation_module(library(xsyntax/xsyntax_tr)).
+:- add_sentence_trans(xsyntax_tr:defunc/3, 610).
+:- add_goal_trans(xsyntax_tr:defunc_goal/2, 610).
+:- endif.
