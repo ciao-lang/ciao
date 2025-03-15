ef3a2a3c74694b8e70ae9c26c585791feb7f9e65
Jose F. Morales <jfmcjf@gmail.com>
2025-03-15 09:35:21 +0100

    (core) xsyntax/statevars as its own package
    Src-commit: af11ad53dc97b89455d1845fc98c300c443a9398

---
+++ core/library/xsyntax/statevars.pl
@@ -0,0 +1,16 @@
+:- package(statevars).
+
+% State variable notation
+
+:- use_package(xsyntax).
+:- use_package(xsyntax/'_xcontrol'). % (needed for statevars and loops)
+
+:- fun_eval statevars(true). % enable expansion of !X syntax
+:- op(60, fx, (!)).
+
+% Reassignment and 'let' notation
+:- op(980, xfx, [(<-)]). % priority between (::) and (,)
+:- op(985, fx, [(let)]). % TODO: check, only meaningful for blocks
+%
+:- fun_eval notation((let Var<-Val), '\6\letvar'(Var,Val)).
+:- fun_eval notation((Var<-Val), '\6\assign'(Var,Val)).
