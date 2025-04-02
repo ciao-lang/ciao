2b45d9ad9fbad0047942d2a2c0647f5b5ba8b1dc
Jose F. Morales <jfmcjf@gmail.com>
2025-04-02 21:26:57 +0200

    (core) fsyntax: lower precedence ':=' so that it can be used for assignment too
    Src-commit: 5ff70599aebd405c3e20e38b9c73f4f655a08ce2

--- core/library/fsyntax/ops.pl
+++ core/library/fsyntax/ops.pl
@@ -2,7 +2,10 @@
 :- op(1150,  fx, (fun_return)).
 % Experiment (related to types) --MH
 % :- op(1150, xfx, (:=)).
-:- op(1130, xfx, (:=)).
+% Old := priority
+% :- op(1130, xfx, (:=)).
+% New := priority
+:- op( 980, xfx, (:=)). % priority between (::) and (,) (must be the same as in statevars.pl)
 :- op(  50,  fx, (~)).
 :- op( 910,  fx, (^^)).
 :- op(  25,  fy, (^)).
