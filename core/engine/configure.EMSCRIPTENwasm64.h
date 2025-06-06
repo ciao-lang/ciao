e5515f4eae6d01fb71354b14dc716f0fee9b319e
Jose F. Morales <jfmcjf@gmail.com>
2025-06-06 19:26:03 +0200

    Ops! Missing file
    Src-commit: ea0a91e75dee094150bc30bd4d21583c920307ff

---
+++ core/engine/configure.EMSCRIPTENwasm64.h
@@ -0,0 +1,17 @@
+/* *** DO NOT EDIT *** */
+/* Output of configure.c for EMSCRIPTENwasm64 platform */
+/* NOTE: Update if configure.c changes */
+
+#define IS_BIG_ENDIAN 0
+#define MallocBase5 0x0
+#define MIN_MEM_ALLOC_5 0x4000
+#define MallocBase4 0x0
+#define MIN_MEM_ALLOC_4 0x4000
+#define MallocBase3 0x0
+#define MIN_MEM_ALLOC_3 0x4000
+#define MallocBase2 0x0
+#define MIN_MEM_ALLOC_2 0x4000
+#define MallocBase1 0x0
+#define MIN_MEM_ALLOC_1 0x4000
+#define MallocBase0 0x0
+#define MIN_MEM_ALLOC_0 0x4000
